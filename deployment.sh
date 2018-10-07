dab2300c7f49dffebb193ad53aa84c1616dfc521
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-10-07 16:53:32 +0500

    deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,11 @@
 # This script is to restart the server in production mode.
 # This will install all the required libraries.
 git pull origin master
+cd client
 npm install
+npm run build
+cd ..
+npm install
+npm install --only=dev
 forever stop server/app.js
 forever start server/app.js
