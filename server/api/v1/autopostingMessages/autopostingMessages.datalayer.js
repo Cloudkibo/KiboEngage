0677f3cff8114ceda0813f9d013a5008199e4124
sania <sania@cloudkibo.com>
2018-10-17 15:20:35 +0500

    #12

--- server/api/v1/autopostingMessages/autopostingMessages.datalayer.js
+++ server/api/v1/autopostingMessages/autopostingMessages.datalayer.js
@@ -1,10 +1,50 @@
 const AutopostingMessages = require('./autopostingMessages.model')
 
+exports.findOneAutopostingMessage = (objectId) => {
+  return AutopostingMessages.findOne({_id: objectId})
+    .exec()
+}
+
+exports.findOneAutopostingMessageUsingQuery = (queryObject) => {
+  return AutopostingMessages.findOne(queryObject)
+    .exec()
+}
+
+exports.findAllAutopostingMessagesUsingQuery = (queryObject) => {
+  return AutopostingMessages.find(queryObject)
+    .exec()
+}
+exports.findAutopostingMessagesUsingQueryWithLimit = (queryObject, limit) => {
+  return AutopostingMessages.find(queryObject).limit(limit)
+    .populate('pageId companyId autopostingId')
+    .exec()
+}
+exports.findAutopostingMessageUsingAggregate = (aggregateObject) => {
+  return AutopostingMessages.aggregate(aggregateObject)
+    .exec()
+}
+exports.findOneAutopostingMessageAndUpdate = (query, update, options) => {
+  return AutopostingMessages.findOneAndUpdate(query, update, options)
+    .exec()
+}
+
+exports.genericFindByIdAndUpdate = (query, updated) => {
+  return AutopostingMessages.findByIdAndUpdate(query, updated, {new: true})
+    .exec()
+}
+exports.genericUpdateAutopostingMessage = (query, updated, options) => {
+  return AutopostingMessages.update(query, updated, options)
+    .exec()
+}
 exports.createAutopostingMessage = (payload) => {
   let obj = new AutopostingMessages(payload)
   return obj.save()
 }
 exports.updateOneAutopostingMessage = (id, payload) => {
   return AutopostingMessages.updateOne({_id: id}, payload)
+    .exec()
+}
+exports.deleteAutopostingMessage = (objectId) => {
+  return AutopostingMessages.deleteOne({_id: objectId})
     .exec()
 }
