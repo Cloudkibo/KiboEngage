fbbccaa541555501131a6961f1c87b04c65101b7
Baqar <bjafri5@gmail.com>
2018-10-18 17:42:48 +0500

    remove unnecessary code and fix errors

--- client/src/socketControllers/controller.js
+++
@@ -1,7 +0,0 @@
-// Import the Socket controller relevant to your module and pass the payload
-import { handleGroupNotifications } from './group.controller'
-
-export const init = (payload) => {
-  handleGroupNotifications(payload)
-  // handleMessageStatus(payload)
-}
