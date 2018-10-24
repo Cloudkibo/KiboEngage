b96da195fe08406845f4edf18eb76e634133620b
sania <sania@cloudkibo.com>
2018-10-24 12:26:00 +0500

    #35, #34

---
+++ server/utility/api.caller.service.js
@@ -0,0 +1,31 @@
+const requestPromise = require('request-promise')
+const config = require('../config/environment/index')
+
+exports.callApi = (endpoint, method = 'get', body, token, type = 'accounts') => {
+  let headers = {
+    'content-type': 'application/json',
+    'Authorization': token
+  }
+  let apiUrl = config.ACCOUNT_URL
+  if (type === 'chat') {
+    apiUrl = config.CHAT_URL
+  }
+  let options = {
+    method: method.toUpperCase(),
+    uri: `${apiUrl}/${endpoint}`,
+    headers,
+    body,
+    json: true
+  }
+  // logger.serverLog(TAG, `requestPromise body ${util.inspect(headers)}`)
+  return requestPromise(options).then(response => {
+    // logger.serverLog(TAG, `response from accounts ${util.inspect(response)}`)
+    return new Promise((resolve, reject) => {
+      if (response.status === 'success') {
+        resolve(response.payload)
+      } else {
+        reject(response.payload)
+      }
+    })
+  })
+}
