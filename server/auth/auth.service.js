bc9ce61c54781d94e4d00971bb4a5c4a14d66596
anishachhatwani <i130315@nu.edu.pk>
2018-10-24 12:46:44 +0500

    integration testing fixes

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -34,7 +34,7 @@ function isAuthenticated () {
           'content-type': 'application/json',
           'Authorization': req.headers.authorization
         }
-        let path = config.API_URL_ACCOUNTS.slice(0, config.API_URL_ACCOUNTS.length - 8)
+        let path = config.ACCOUNTS_URL.slice(0, config.ACCOUNTS_URL.length - 8)
         console.log('path in auth.service', path)
         let options = {
           method: 'GET',
