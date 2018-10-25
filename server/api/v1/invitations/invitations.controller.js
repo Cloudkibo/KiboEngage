591ed23cb0e1603f9f3b6965d322419421e82142
Baqar <bjafri5@gmail.com>
2018-10-25 10:06:00 +0500

    integration fixes

--- server/api/v1/invitations/invitations.controller.js
+++ server/api/v1/invitations/invitations.controller.js
@@ -1,5 +1,4 @@
 
-
 const logger = require('../../../components/logger')
 const TAG = 'api/invitations/invitations.controller.js'
 const InvitationsDataLayer = require('./invitations.datalayer.js')
@@ -7,7 +6,7 @@ const InviteAgentTokenDataLayer = require('../inviteagenttoken/inviteagenttoken.
 const callApi = require('../utility')
 
 exports.index = function (req, res) {
-  callApi.callApi('companyuser/query', post, {domain_email: req.user.domain_email})
+  callApi.callApi('companyuser/query', 'post', {domain_email: req.user.domain_email})
     .then(companyUser => {
       if (!companyUser) {
         return res.status(404).json({
@@ -15,7 +14,7 @@ exports.index = function (req, res) {
           description: 'The user account does not belong to any company. Please contact support'
         })
       }
-      IpCountryDataLayer.findOneInvitationObjectUsingQuery({companyId: companyUser.companyId})
+      InvitationsDataLayer.findOneInvitationObjectUsingQuery({companyId: companyUser.companyId})
         .then(invitations => {
           res.status(200).json({
             status: 'success',
@@ -38,7 +37,7 @@ exports.index = function (req, res) {
 }
 
 exports.cancel = function (req, res) {
-  callApi.callApi('companyuser/query', post, {domain_email: req.user.domain_email})
+  callApi.callApi('companyuser/query', 'post', {domain_email: req.user.domain_email})
     .then(companyUser => {
       if (!companyUser) {
         return res.status(404).json({
@@ -49,18 +48,18 @@ exports.cancel = function (req, res) {
       InvitationsDataLayer.removeInvitationObjectUsingQuery({email: req.body.email, companyId: companyUser.companyId})
         .then(() => {
           InviteAgentTokenDataLayer.removeInviteAgentTokenUsingQuery({email: req.body.email, companyId: companyUser.companyId})
-          .then(() => {
-            res.status(200).json({
-              status: 'success',
-              description: 'Invitation has been cancelled.'
+            .then(() => {
+              res.status(200).json({
+                status: 'success',
+                description: 'Invitation has been cancelled.'
+              })
             })
-          })
-          .catch(err => {
-            return res.status(500).json({
-              status: 'failed',
-              description: `Internal Server Error ${JSON.stringify(err)}`
+            .catch(err => {
+              return res.status(500).json({
+                status: 'failed',
+                description: `Internal Server Error ${JSON.stringify(err)}`
+              })
             })
-          })
         })
         .catch(err => {
           return res.status(500).json({
