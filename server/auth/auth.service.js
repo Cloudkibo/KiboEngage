0d48a17a8be18f07f1ada03e8cb1cf0fcd9113f3
Baqar <bjafri5@gmail.com>
2018-10-25 13:18:48 +0500

    plan issue

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -112,11 +112,9 @@ function doesPlanPermitsThisAction (action) {
   if (!action) throw new Error('Action needs to be set')
 
   return compose().use(function meetsRequirements (req, res, next) {
-    console.log('user: ', JSON.stringify(req.user.plan))
-    apiCaller.callApi(`featureUsage/planQuery`, 'post', {planId: req.user.plan.plan_id._id}, req.headers.authorization)
+    apiCaller.callApi(`permissions_plan/query`, 'post', {plan_id: req.user.plan.plan_id._id}, req.headers.authorization)
       .then(plan => {
         plan = plan[0]
-        console.log(`featureUsage plan: ${JSON.stringify(plan)}`)
         if (!plan) {
           return res.status(500)
             .json({
@@ -125,10 +123,8 @@ function doesPlanPermitsThisAction (action) {
             })
         }
         if (req.user && req.user.plan && plan[action]) {
-          console.log(`plan permits ${action}`)
           next()
         } else {
-          console.log(`plan does not permit ${action}`)
           res.status(403)
             .json({
               status: 'failed',
