a59e19e60e7672c0ea5cda327923acfbb43fedea
Arveen kumar <arveen@cloudkibo.com>
2018-10-30 12:59:54 +0500

    surveyLogicLayer

--- server/api/v1/surveys/surveys.logiclayer.js
+++ server/api/v1/surveys/surveys.logiclayer.js
@@ -1,48 +1,48 @@
 const mongoose = require('mongoose')
 let _ = require('lodash')
 exports.getCriterias = function (body, companyUser) {
-    let startDate = new Date() // Current date
-    startDate.setDate(startDate.getDate() - body.days)
-    startDate.setHours(0) // Set the hour, minute and second components to 0
-    startDate.setMinutes(0)
-    startDate.setSeconds(0)
-    let findCriteria = {
-      companyId: companyUser.companyId,
-      'datetime': body.days !== '0' ? {
-        $gte: startDate
-      } : {$exists: true}
-    }
-    let finalCriteria = {}
-    let recordsToSkip = 0
-    if (body.first_page === 'first') {
-      finalCriteria = [
-        { $match: findCriteria },
-        { $sort: {datetime: -1} },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    } else if (body.first_page === 'next') {
-      recordsToSkip = Math.abs(((body.requested_page - 1) - (body.current_page))) * body.number_of_records
-      finalCriteria = [
-        { $match: { $and: [findCriteria, { _id: { $lt: mongoose.Types.ObjectId(body.last_id) } }] } },
-        { $sort: {datetime: -1} },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    } else if (body.first_page === 'previous') {
-      recordsToSkip = Math.abs(((body.requested_page) - (body.current_page - 1))) * body.number_of_records
-      finalCriteria = [
-        { $match: { $and: [findCriteria, { _id: { $gt: mongoose.Types.ObjectId(body.last_id) } }] } },
-        { $sort: {datetime: 1} },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    }
-    let countCriteria = [
+  let startDate = new Date() // Current date
+  startDate.setDate(startDate.getDate() - body.days)
+  startDate.setHours(0) // Set the hour, minute and second components to 0
+  startDate.setMinutes(0)
+  startDate.setSeconds(0)
+  let findCriteria = {
+    companyId: mongoose.Types.ObjectId(companyUser.companyId),
+    'datetime': body.days !== '0' ? {
+      $gte: startDate
+    } : {$exists: true}
+  }
+  let finalCriteria = {}
+  let recordsToSkip = 0
+  if (body.first_page === 'first') {
+    finalCriteria = [
       { $match: findCriteria },
-      { $group: { _id: null, count: { $sum: 1 } } }
+      { $sort: {datetime: -1} },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
     ]
-    return {countCriteria: countCriteria, fetchCriteria: finalCriteria}
+  } else if (body.first_page === 'next') {
+    recordsToSkip = Math.abs(((body.requested_page - 1) - (body.current_page))) * body.number_of_records
+    finalCriteria = [
+      { $match: { $and: [findCriteria, { _id: { $lt: mongoose.Types.ObjectId(body.last_id) } }] } },
+      { $sort: {datetime: -1} },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
+    ]
+  } else if (body.first_page === 'previous') {
+    recordsToSkip = Math.abs(((body.requested_page) - (body.current_page - 1))) * body.number_of_records
+    finalCriteria = [
+      { $match: { $and: [findCriteria, { _id: { $gt: mongoose.Types.ObjectId(body.last_id) } }] } },
+      { $sort: {datetime: 1} },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
+    ]
+  }
+  let countCriteria = [
+    { $match: findCriteria },
+    { $group: { _id: null, count: { $sum: 1 } } }
+  ]
+  return {countCriteria: countCriteria, fetchCriteria: finalCriteria}
   }
 
 exports.createSurveyPayload = function (req, companyUser) {
