a584c88eb399f8fce8984efcc5120d43376bb4e6
sania <sania@cloudkibo.com>
2018-11-01 11:35:31 +0500

    Automation Queue logs

--- server/api/v1/automationQueue/automationQueue.controller.js
+++ server/api/v1/automationQueue/automationQueue.controller.js
@@ -4,18 +4,18 @@ exports.index = function (req, res) {
   AutomationQueue.findAllAutomationQueueObjectsUsingQuery({ companyId: req.body.companyId })
     .then(automationQueue => {
       if (!automationQueue) {
-        return res.status(404).json({
+        res.status(404).json({
           status: 'failed',
           description: 'Automation Queue is empty for this company. Please contact support'
         })
       }
-      return res.status(200).json({
+      res.status(200).json({
         status: 'success',
         payload: automationQueue
       })
     })
     .catch(err => {
-      return res.status(500).json({
+      res.status(500).json({
         status: 'failed',
         description: `Internal Server Error in fetching automation queues against companyId${JSON.stringify(err)}`
       })
@@ -26,13 +26,13 @@ exports.create = function (req, res) {
   AutomationQueue.createAutomationQueueObject(req.body.payload)
     .then(automationQueueObject => {
       console.log('Automation Queue Object Create', automationQueueObject)
-      return res.status(200).json({
+      res.status(200).json({
         status: 'Automation queue object saved',
         payload: automationQueueObject
       })
     })
     .catch(err => {
-      return res.status(500).json({
+      res.status(500).json({
         status: 'failed',
         description: `Internal Server Error in saving automation queue object ${JSON.stringify(err)}`
       })
