65b3960997ef6be97c5dab3050ca291944f6d68e
sania <sania@cloudkibo.com>
2018-11-05 20:25:06 +0500

    #31

--- server/api/v1/page_broadcast/page_broadcast.datalayer.js
+++ server/api/v1/page_broadcast/page_broadcast.datalayer.js
@@ -13,10 +13,7 @@ exports.genericFind = (query) => {
   return PageBroadcast.find(query)
     .exec()
 }
-exports.genericFindOne = (query) => {
-  return PageBroadcast.findOne(query)
-    .exec()
-}
+
 exports.createForBroadcastPage = (payload) => {
   let obj = new PageBroadcast(payload)
   return obj.save()
