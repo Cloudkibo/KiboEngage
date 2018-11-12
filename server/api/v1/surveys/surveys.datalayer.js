1bf1a80816cd3cc4619c0f26d5c66a07a6ea3a96
arveen kumar <arveenkumar55@gmail.com>
2018-11-12 16:30:46 +0500

    logs

--- server/api/v1/surveys/surveys.datalayer.js
+++ server/api/v1/surveys/surveys.datalayer.js
@@ -75,6 +75,6 @@ exports.createSurvey = (survey) => {
   return Surveys.create(survey)
 } 
 exports.QuestionfindSurveyById = (req) => {
-  return   Surveys.findOne({_id: req.body._id}
+  return   Surveys.findOne({_id: req.body._id})
   .exec()
 }
