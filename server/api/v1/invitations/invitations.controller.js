82cf4dc72eeeada28e0384618b7dd5ef84458b02
sania <sania@cloudkibo.com>
2018-11-23 11:07:17 +0500

    #125

--- server/api/v1/invitations/invitations.controller.js
+++ server/api/v1/invitations/invitations.controller.js
@@ -1,8 +1,6 @@
 
 const logger = require('../../../components/logger')
 const TAG = 'api/invitations/invitations.controller.js'
-const InvitationsDataLayer = require('./invitations.datalayer.js')
-const InviteAgentTokenDataLayer = require('../inviteagenttoken/inviteagenttoken.datalayer')
 const callApi = require('../utility')
 
 exports.index = function (req, res) {
@@ -23,36 +21,13 @@ exports.index = function (req, res) {
 }
 
 exports.cancel = function (req, res) {
-  callApi.callApi('companyuser/query', 'post', {domain_email: req.user.domain_email})
-    .then(companyUser => {
-      if (!companyUser) {
-        return res.status(404).json({
-          status: 'failed',
-          description: 'The user account does not belong to any company. Please contact support'
-        })
-      }
-      InvitationsDataLayer.removeInvitationObjectUsingQuery({email: req.body.email, companyId: companyUser.companyId})
-        .then(() => {
-          InviteAgentTokenDataLayer.removeInviteAgentTokenUsingQuery({email: req.body.email, companyId: companyUser.companyId})
-            .then(() => {
-              res.status(200).json({
-                status: 'success',
-                description: 'Invitation has been cancelled.'
-              })
-            })
-            .catch(err => {
-              return res.status(500).json({
-                status: 'failed',
-                description: `Internal Server Error ${JSON.stringify(err)}`
-              })
-            })
-        })
-        .catch(err => {
-          return res.status(500).json({
-            status: 'failed',
-            description: `Internal Server Error ${JSON.stringify(err)}`
-          })
-        })
+  callApi.callApi('invitations/cancel', 'post', {email: req.body.email}, req.headers.authorization)
+    .then(result => {
+      console.log('result', result)
+      res.status(200).json({
+        status: 'success',
+        description: 'Invitation has been cancelled.'
+      })
     })
     .catch(err => {
       return res.status(500).json({
