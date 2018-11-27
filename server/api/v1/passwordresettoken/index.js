1f821a7763ca9a4b8c1dda104ef15e5d0078b551
Arveen kumar <arveen@cloudkibo.com>
2018-11-27 13:58:10 +0500

    password

--- server/api/v1/passwordresettoken/index.js
+++ server/api/v1/passwordresettoken/index.js
@@ -7,9 +7,5 @@ let auth = require('../../../auth/auth.service')
 let router = express.Router()
 
 router.post('/change', auth.isAuthenticated(), controller.change)
-router.post('/forgot', controller.forgot)
-router.post('/forgotWorkspaceName', controller.forgotWorkspaceName)
-router.post('/reset', controller.reset)
-router.get('/verify/:id', controller.verify)
 
 module.exports = router
