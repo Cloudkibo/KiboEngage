a40d8579726861d8fb2021ae2e9e166eaf89464a
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-11-30 14:45:29 +0500

    Update controller.js

--- server/api/scripts/controller.js
+++ server/api/scripts/controller.js
@@ -1,7 +1,7 @@
 const TAG = 'api/scripts/controller'
-const BroadcastPageDataLayer = require('../page_broadcast/page_broadcast.datalayer')
-const PollPageDataLayer = require('../page_poll/page_poll.datalayer')
-const SurveyPageDataLayer = require('../page_survey/page_survey.datalayer')
+const BroadcastPageDataLayer = require('../v1/page_broadcast/page_broadcast.datalayer')
+const PollPageDataLayer = require('../v1/page_poll/page_poll.datalayer')
+const SurveyPageDataLayer = require('../v1/page_survey/page_survey.datalayer')
 
 exports.normalizeDataForDelivery = function (req, res) {
   BroadcastPageDataLayer.update({sent: null}, {sent: true}, {multi: true}).exec()
