933acc82d50c87d10016ebd3b49e60d565c8406e
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-11-30 14:31:35 +0500

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,6 @@
 # This script is to restart the server in production mode.
 # This will install all the required libraries.
 git pull origin master
-npm install -g forever
 npm install
-npm install --only=dev
 forever stop server/app.js
 forever start server/app.js
