490d5f65c5fa8eda82f8112833f314f8e99f5c45
Imran Shoukat <imran.shoukat1995@gmail.com>
2018-11-30 14:37:14 +0500

    Update index.js

--- server/api/scripts/index.js
+++ server/api/scripts/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('./controller')
 
-router.post('/normalizeDataForDelivery', controller.normalizeDataForDelivery)
+router.get('/normalizeDataForDelivery', controller.normalizeDataForDelivery)
 
 module.exports = router
