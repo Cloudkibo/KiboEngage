1ce9d9f32e6f5b23c7b80c4c33693b10fa40060c
anishachhatwani <i130315@nu.edu.pk>
2018-12-09 17:17:10 +0500

    templates

--- server/api/v1.1/templates/surveyQuestion.datalayer.js
+++ server/api/v1.1/templates/surveyQuestion.datalayer.js
@@ -1,5 +1,6 @@
 const { callApi } = require('../utility')
 
 exports.createQuestionSurveys = (surveyQuestion) => {
+  console.log('surveyQuestion', surveyQuestion)
   return callApi(`templates/survey/question`, 'post', surveyQuestion, '', 'kiboengage')
 }
