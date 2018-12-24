080ace1dbb9b370d54db1615ff4a71bc05f2debc
anishachhatwani <i130315@nu.edu.pk>
2018-12-24 13:44:39 +0500

    subscription permission

--- server/api/v1.1/landingPage/landingPage.controller.js
+++ server/api/v1.1/landingPage/landingPage.controller.js
@@ -106,7 +106,7 @@ exports.delete = function (req, res) {
     .then(landingPages => {
       let landingPage = landingPages[0]
       console.log('landingPage', landingPage)
-      utility.callApi(`landingPage/landingPageState/${landingPage.initialState}`, 'delete', {}, req.headers.authorization)
+      utility.callApi(`landingPage/landingPageState/${landingPage.initialState._id}`, 'delete', {}, req.headers.authorization)
         .then(result => {
           if (landingPage.submittedState && landingPage.submittedState.state) {
             utility.callApi(`landingPage/landingPageState/${landingPage.submittedState.state}`, 'delete', {}, req.headers.authorization)
