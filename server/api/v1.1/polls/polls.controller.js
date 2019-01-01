2471b39b6c96239aae2a1625829849fe995b1416
anishachhatwani <i130315@nu.edu.pk>
2019-01-01 14:20:23 +0500

    poll survey

--- server/api/v1.1/polls/polls.controller.js
+++ server/api/v1.1/polls/polls.controller.js
@@ -221,7 +221,7 @@ exports.send = function (req, res) {
                   utility.callApi(`pages/query`, 'post', {companyId: companyUser.companyId, connected: true}, req.headers.authorization)
                     .then(userPage => {
                       userPage = userPage[0]
-                      utility.callApi(`user/query`, 'post', {_id: userPage[0].userId}, req.headers.authorization)
+                      utility.callApi(`user/query`, 'post', {_id: userPage.userId}, req.headers.authorization)
                         .then(connectedUser => {
                           connectedUser = connectedUser[0]
                           var currentUser
