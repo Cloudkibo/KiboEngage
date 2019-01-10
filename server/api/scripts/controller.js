b8b173971039d3b15336964a6de1c035ad6b6759
anishachhatwani <i130315@nu.edu.pk>
2019-01-10 10:47:03 +0500

    add whitelist domain

--- server/api/scripts/controller.js
+++ server/api/scripts/controller.js
@@ -2,6 +2,8 @@ const TAG = 'api/scripts/controller'
 const BroadcastPageDataLayer = require('../v1/page_broadcast/page_broadcast.datalayer')
 const PollPageDataLayer = require('../v1/page_poll/page_poll.datalayer')
 const SurveyPageDataLayer = require('../v1/page_survey/page_survey.datalayer')
+const utility = require('../v1.1/utility')
+let config = require('../../config/environment')
 
 exports.normalizeDataForDelivery = function (req, res) {
   BroadcastPageDataLayer.genericUpdate({sent: null}, {sent: true}, {multi: true})
@@ -26,4 +28,32 @@ exports.normalizeDataForDelivery = function (req, res) {
       console.log(TAG, `Survey sent normalized failed ${err}`)
     })
   return res.status(200).json({status: 'success', payload: 'Data has been normalized successfully!'})
+}
+
+exports.addWhitelistDomain = function (req, res) {
+  utility.callApi(`pages/query`, 'post', {connected: true}, req.headers.authorization) // fetch connected pages
+    .then(pages => {
+      for (let i = 0; i < pages.length; i++) {
+        utility.callApi(`pages/whitelistDomain`, 'post', {page_id: pages[i].pageId, whitelistDomains: [`${config.domain}`]}, req.headers.authorization)
+          .then(whitelistDomains => {
+            return res.status(200).json({
+              status: 'success',
+              payload: whitelistDomains
+            })
+          })
+          .catch(error => {
+            return res.status(500).json({
+              status: 'failed',
+              description: `Failed to save whitelist domains ${JSON.stringify(error)}`
+            })
+          })
+      }
+    })
+    .catch(error => {
+      return res.status(500).json({
+        status: 'failed',
+        payload: `Failed to fetch connected pages ${JSON.stringify(error)}`
+      })
+    })
+  return res.status(200).json({status: 'success', payload: 'Domain has been whitelisted'})
 }
