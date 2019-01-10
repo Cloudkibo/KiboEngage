fdcbf2f27d2946e6f29d543a603148b56d2d8318
anishachhatwani <i130315@nu.edu.pk>
2019-01-10 11:52:10 +0500

    add whitelist domain

--- server/api/scripts/controller.js
+++ server/api/scripts/controller.js
@@ -4,6 +4,7 @@ const PollPageDataLayer = require('../v1/page_poll/page_poll.datalayer')
 const SurveyPageDataLayer = require('../v1/page_survey/page_survey.datalayer')
 const utility = require('../v1.1/utility')
 let config = require('../../config/environment')
+const needle = require('needle')
 
 exports.normalizeDataForDelivery = function (req, res) {
   BroadcastPageDataLayer.genericUpdate({sent: null}, {sent: true}, {multi: true})
@@ -35,12 +36,43 @@ exports.addWhitelistDomain = function (req, res) {
     .then(pages => {
       console.log('pages fetched in script', pages.length)
       for (let i = 0; i < pages.length; i++) {
-        utility.callApi(`pages/whitelistDomain`, 'post', {page_id: pages[i].pageId, whitelistDomains: [`${config.domain}`]}, req.headers.authorization)
-          .then(whitelistDomains => {
-            console.log('whitelistDomains', whitelistDomains)
+        utility.callApi(`user/query`, 'post', {_id: pages[i].userId}, req.headers.authorization)
+          .then(connectedUser => {
+            connectedUser = connectedUser[0]
+            if (connectedUser.facebookInfo) {
+              needle.get(`https://graph.facebook.com/v2.10/${pages[i].pageId}?fields=access_token&access_token=${connectedUser.facebookInfo.fbToken}`,
+                (err, resp) => {
+                  if (err) {
+                    console.log('error in getting page access token', err)
+                  }
+                  var accessToken = resp.body.access_token
+                  needle.get(`https://graph.facebook.com/v2.6/me/messenger_profile?fields=whitelisted_domains&access_token=${accessToken}`, function (err, resp) {
+                    if (err) {
+                      console.log('error in whitelisted_domains', err)
+                    }
+                    var body = JSON.parse(JSON.stringify(resp.body))
+                    let temp = []
+                    if (body.data && body.data.length > 0 && body.data[0].whitelisted_domains) {
+                      temp = body.data[0].whitelisted_domains
+                    }
+                    temp.push(`${config.domain}`)
+                    let whitelistedDomains = {
+                      whitelisted_domains: temp
+                    }
+                    console.log('whitelistdomains', whitelistedDomains)
+                    let requesturl = `https://graph.facebook.com/v2.6/me/messenger_profile?access_token=${accessToken}`
+                    needle.request('post', requesturl, whitelistedDomains, {json: true}, function (err, resp) {
+                      if (err) {
+                        console.log('error in whitelisted_domains', err)
+                      }
+                      console.log('response from whitelisted_domains', resp.body)
+                    })
+                  })
+                })
+            }
           })
           .catch(error => {
-            console.log('error in whitelisting domain', error)
+            console.log('Failed to fetch user', error)
           })
       }
     })
