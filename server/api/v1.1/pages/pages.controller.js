d6ff9ad9c5f5cd9da87909897ccc1fffbd999724
arveen kumar <arveenkumar55@gmail.com>
2019-01-11 12:21:25 +0500

    connectedpage

--- server/api/v1.1/pages/pages.controller.js
+++ server/api/v1.1/pages/pages.controller.js
@@ -305,7 +305,7 @@ exports.enable = function (req, res) {
                                 } else {
                                   res.status(400).json({
                                     status: 'failed',
-                                    payload: `Page is already connected by ${pageConnected.userId.facebookInfo.name}. In order to manage this page please ask ${pageConnected.userId.facebookInfo.name} to create a team account and invite you.`
+                                    payload: `Page is already connected by ${pageConnected[0].userId.facebookInfo.name}. In order to manage this page please ask ${pageConnected[0].userId.facebookInfo.name} to create a team account and invite you.`
                                   })
                                 }
                               })
