d09d7ca1cccbaf3c3351174ba0ecb4d81991c8ff
sania <sania@cloudkibo.com>
2019-01-14 14:30:24 +0500

    kiboengage #192

--- server/api/v1.1/pages/index.js
+++ server/api/v1.1/pages/index.js
@@ -60,7 +60,7 @@ router.get('/fetchWhitelistedDomains/:_id',
   auth.isAuthenticated(),
   controller.fetchWhitelistedDomains)
 
-router.post('isWhitelisted',
+router.post('/isWhitelisted',
   auth.isAuthenticated(),
   controller.isWhitelisted)
 
