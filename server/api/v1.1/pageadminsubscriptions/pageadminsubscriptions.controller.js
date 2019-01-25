b39947a2124fe9f309b5bf6ba531ae7991251010
sania <sania@cloudkibo.com>
2019-01-25 10:27:37 +0500

    KiboPush #4334

--- server/api/v1.1/pageadminsubscriptions/pageadminsubscriptions.controller.js
+++ server/api/v1.1/pageadminsubscriptions/pageadminsubscriptions.controller.js
@@ -44,30 +44,19 @@ exports.create = function (req, res) {
   logger.serverLog('', `payload ${JSON.stringify(payload)}`)
   PageAdminSubscriptionsDataLayer.create(payload)
     .then(updatedRecord => {
-      utility.callApi(`pages/query`, 'post', { pageId: req.body.pageId, companyId: req.body.companyId }, 'accounts')
-        .then(pages => {
-          logger.serverLog('', `page ${JSON.stringify(pages)}`)
-          let page = pages[0]
-          require('./../../../config/socketio').sendMessageToClient({
-            room_id: updatedRecord.companyId,
-            body: {
-              action: 'admin_subscriber',
-              payload: {
-                subscribed_page: page
-              }
-            }
-          })
-          return res.status(200).json({
-            status: 'success',
-            payload: updatedRecord
-          })
-        })
-        .catch(err => {
-          return res.status(500).json({
-            status: 'failed',
-            description: `Internal Server Error in Getting pages${JSON.stringify(err)}`
-          })
-        })
+      require('./../../../config/socketio').sendMessageToClient({
+        room_id: updatedRecord.companyId,
+        body: {
+          action: 'admin_subscriber',
+          payload: {
+            subscribed_page: req.body.pageId
+          }
+        }
+      })
+      return res.status(200).json({
+        status: 'success',
+        payload: updatedRecord
+      })
     })
     .catch(err => {
       return res.status(500).json({
