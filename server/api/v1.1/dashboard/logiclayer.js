5c4b5eb79d936614b5856f4206426dfacf42a1a0
sania <sania@cloudkibo.com>
2019-01-31 14:27:50 +0500

    Logs

--- server/api/v1.1/dashboard/logiclayer.js
+++ server/api/v1.1/dashboard/logiclayer.js
@@ -1,6 +1,6 @@
-exports.getCriterias = function (body, companyUser, seen) {
+exports.getCriterias = function (body, companyUser, seen, pageIds) {
   let matchAggregate = { companyId: companyUser.companyId.toString(),
-    'pageId': body.pageId === 'all' ? { $exists: true } : body.pageId,
+    'pageId': body.pageId === 'all' ? {$in: pageIds} : body.pageId,
     'datetime': body.days === 'all' ? { $exists: true } : {
       $gte: new Date(
         (new Date().getTime() - (body.days * 24 * 60 * 60 * 1000))),
@@ -13,41 +13,55 @@ exports.getCriterias = function (body, companyUser, seen) {
 }
 
 exports.queryForSubscribers = function (body, companyUser, isSubscribed) {
-  let query = [ {$match: { companyId: companyUser.companyId,
-    'datetime': body.days === 'all' ? { $exists: true } : {
-      $gte: new Date(
-        (new Date().getTime() - (body.days * 24 * 60 * 60 * 1000))),
-      $lt: new Date(
-        (new Date().getTime()))
+  let query = [
+    { $lookup: {from: 'pages', localField: 'pageId', foreignField: '_id', as: 'pageId'} },
+    { $unwind: '$pageId' },
+    {$match: { companyId: companyUser.companyId,
+      'datetime': body.days === 'all' ? { $exists: true } : {
+        $gte: new Date(
+          (new Date().getTime() - (body.days * 24 * 60 * 60 * 1000))),
+        $lt: new Date(
+          (new Date().getTime()))
+      },
+      isSubscribed: isSubscribed
+    }
     },
-    'pageId': body.pageId === 'all' ? { $exists: true } : body.pageId,
-    isSubscribed: isSubscribed
-  }
-  },
-  {$group: {
-    _id: null,
-    count: {$sum: 1}}
-  }
+    {$group: {
+      _id: null,
+      count: {$sum: 1}}
+    }
   ]
+  if (body.pageId === 'all') {
+    query[0].$match.pageId.connected = true
+  } else {
+    query[0].$match.pageId._id = body.pageId
+  }
   return query
 }
 
 exports.queryForSubscribersGraph = function (body, companyUser, isSubscribed) {
-  let query = [ {$match: { companyId: companyUser.companyId,
-    'datetime': body.days === 'all' ? { $exists: true } : {
-      $gte: new Date(
-        (new Date().getTime() - (body.days * 24 * 60 * 60 * 1000))),
-      $lt: new Date(
-        (new Date().getTime()))
+  let query = [
+    { $lookup: {from: 'pages', localField: 'pageId', foreignField: '_id', as: 'pageId'} },
+    { $unwind: '$pageId' },
+    {$match: { companyId: companyUser.companyId,
+      'datetime': body.days === 'all' ? { $exists: true } : {
+        $gte: new Date(
+          (new Date().getTime() - (body.days * 24 * 60 * 60 * 1000))),
+        $lt: new Date(
+          (new Date().getTime()))
+      },
+      isSubscribed: isSubscribed
+    }
     },
-    'pageId': body.pageId === 'all' ? { $exists: true } : body.pageId,
-    isSubscribed: isSubscribed
-  }
-  },
-  {$group: {
-    _id: {'year': {$year: '$datetime'}, 'month': {$month: '$datetime'}, 'day': {$dayOfMonth: '$datetime'}},
-    count: {$sum: 1}}
-  }
+    {$group: {
+      _id: {'year': {$year: '$datetime'}, 'month': {$month: '$datetime'}, 'day': {$dayOfMonth: '$datetime'}},
+      count: {$sum: 1}}
+    }
   ]
+  if (body.pageId === 'all') {
+    query[0].$match.pageId.connected = true
+  } else {
+    query[0].$match.pageId._id = body.pageId
+  }
   return query
 }
