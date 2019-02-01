85314b2f5025d98e6582405c68ddeca7d5555a89
Baqar <bjafri5@gmail.com>
2019-02-01 13:50:37 +0500

    remove development environment from sentry

--- server/app.js
+++ server/app.js
@@ -5,14 +5,17 @@ const config = require('./config/environment/index')
 
 const app = express()
 const httpApp = express()
-const Raven = require('raven')
-
-Raven.config('https://6c7958e0570f455381d6f17122fbd117:d2041f4406ff4b3cb51290d9b8661a7d@sentry.io/292307', {
-  environment: config.env,
-  parseUser: ['name', 'email', 'domain', 'role', 'emailVerified']
-}).install()
 
 const appObj = (config.env === 'production' || config.env === 'staging') ? app : httpApp
+
+if (config.env === 'production' || config.env === 'staging') {
+    const Raven = require('raven')
+    Raven.config('https://6c7958e0570f455381d6f17122fbd117:d2041f4406ff4b3cb51290d9b8661a7d@sentry.io/292307', {
+      environment: config.env,
+      parseUser: ['name', 'email', 'domain', 'role', 'emailVerified']
+    }).install()
+    appObj.use(Raven.requestHandler())
+}
 
 appObj.use(Raven.requestHandler())
 
