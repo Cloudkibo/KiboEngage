b146ae3e1166c0dac18f6ed2b16aea37132d10df
sania <sania@cloudkibo.com>
2019-02-16 15:14:43 +0500

    Revert push changes

--- server/routes.js
+++ server/routes.js
@@ -47,7 +47,6 @@ module.exports = function (app) {
   app.use('/api/scripts', require('./api/v1.1/scripts'))
   app.use('/api/custom_fields', require('./api/v1.1/custom_fields'))
   app.use('/api/custom_field_subscribers/', require('./api/v1.1/custom_field_subscribers'))
-  app.use('/api/pushUsingPhoneNumber/', require('./api/v1.1/pushUsingPhoneNumber'))
 
   // auth middleware go here if you authenticate on same server
   app.use('/auth', require('./auth'))
