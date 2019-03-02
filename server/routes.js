65d1889dcbad064b15b2eace503d6a9a1bd98e36
Muzamil <muzamil.bukhari.39@gmail.com>
2019-03-02 13:23:44 +0500

    Added url check in logic layer

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,6 @@
 const config = require('./config/environment/index')
 const { callApi } = require('./api/v1.1/utility')
 const logger = require('./components/logger')
-const utility = require('./components/utility')
 const TAG = 'LandingPage'
 const Raven = require('raven')
 const path = require('path')
@@ -71,7 +70,6 @@ module.exports = function (app) {
         landingPage.state = landingPages[0].initialState
         landingPage.facebookClientId = config.facebook.clientID
         landingPage.currentState = 'initial'
-        landingPage.setProtocolUrl = utility.setProtocolUrl
         res.render('landingPage', { landingPage })
       })
       .catch(err => {
