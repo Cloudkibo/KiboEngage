7a837b7f772455e09d1d102e9bdd439781b35fae
Imran Shoukat <ishoukat@192.168.0.108>
2019-03-15 14:33:28 +0500

    modify send broadcast

---
+++ server/api/global/broadcastApi.js
@@ -0,0 +1,55 @@
+const prepareMessageData = require('./prepareMessageData')
+const { facebookApiCaller } = require('./facebookApiCaller')
+
+exports.callBroadcastMessagesEndpoint = (messageCreativeId, labels, pageAccessToken) => {
+  let data = {
+    'message_creative_id': messageCreativeId,
+    'notification_type': 'REGULAR',
+    'messaging_type': 'MESSAGE_TAG',
+    'tag': 'NON_PROMOTIONAL_SUBSCRIPTION',
+    'targetting': {
+      'labels': {
+        'operator': 'AND',
+        'values': labels
+      }
+    }
+  }
+  facebookApiCaller('v2.11', `me/broadcast_messages?access_token=${pageAccessToken}`, 'post', data)
+    .then(response => {
+      if (response.broadcast_id) {
+        return {status: 'success', broadcast_id: response.broadcast_id}
+      } else {
+        return {status: 'failed', description: response.error}
+      }
+    })
+    .catch(err => {
+      return err
+    })
+}
+
+exports.callMessageCreativesEndpoint = (payload, pageAccessToken) => {
+  let data = {
+    'messages': getMessagesData(payload)
+  }
+  facebookApiCaller('v2.11', `me/message_creatives?access_token=${pageAccessToken}`, 'post', data)
+    .then(response => {
+      if (response.message_creative_id) {
+        return {status: 'success', message_creative_id: response.message_creative_id}
+      } else {
+        return {status: 'failed', description: response.error}
+      }
+    })
+    .catch(err => {
+      return err
+    })
+}
+
+function getMessagesData (payload) {
+  let messages = []
+  payload.forEach((item, i) => {
+    messages.push(prepareMessageData.facebook(payload, '{{first_name}}', '{{last_name}}'))
+    if (i === messages.length - 1) {
+      return messages
+    }
+  })
+}
---
+++ server/api/global/prepareMessageData.js
@@ -0,0 +1,160 @@
+exports.facebook = (body, fname, lname) => {
+  let payload = {}
+  let text = body.text
+  if (body.componentType === 'text' && !body.buttons) {
+    if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
+      text = text.replace(
+        '{{user_full_name}}', fname + ' ' + lname)
+    }
+    if (body.text.includes('{{user_first_name}}')) {
+      text = text.replace(
+        '{{user_first_name}}', fname)
+    }
+    if (body.text.includes('{{user_last_name}}')) {
+      text = text.replace(
+        '{{user_last_name}}', lname)
+    }
+    payload = {
+      'text': text,
+      'metadata': 'This is a meta data'
+    }
+    return payload
+  } else if (body.componentType === 'text' && body.buttons) {
+    if (body.text.includes('{{user_full_name}}') || body.text.includes('[Username]')) {
+      text = text.replace(
+        '{{user_full_name}}', fname + ' ' + lname)
+    }
+    if (body.text.includes('{{user_first_name}}')) {
+      text = text.replace(
+        '{{user_first_name}}', fname)
+    }
+    if (body.text.includes('{{user_last_name}}')) {
+      text = text.replace(
+        '{{user_last_name}}', lname)
+    }
+    payload = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'button',
+          'text': text,
+          'buttons': body.buttons
+        }
+      }
+    }
+  } else if (['image', 'audio', 'file', 'video'].indexOf(
+    body.componentType) > -1) {
+    payload = {
+      'attachment': {
+        'type': body.componentType,
+        'payload': {
+          'attachment_id': body.fileurl.attachment_id
+        }
+      }
+    }
+    return payload
+    // todo test this one. we are not removing as we need to keep it for live chat
+    // if (!isForLiveChat) deleteFile(body.fileurl)
+  } else if (['gif', 'sticker', 'thumbsUp'].indexOf(
+    body.componentType) > -1) {
+    payload = {
+      'attachment': {
+        'type': 'image',
+        'payload': {
+          'url': body.fileurl
+        }
+      }
+    }
+  } else if (body.componentType === 'card') {
+    if (body.default_action) {
+      payload = {
+        'attachment': {
+          'type': 'template',
+          'payload': {
+            'template_type': 'generic',
+            'elements': [
+              {
+                'title': body.title,
+                'image_url': body.image_url,
+                'subtitle': body.description,
+                'buttons': body.buttons,
+                'default_action': body.default_action
+              }
+            ]
+          }
+        }
+      }
+    } else {
+      payload = {
+        'attachment': {
+          'type': 'template',
+          'payload': {
+            'template_type': 'generic',
+            'elements': [
+              {
+                'title': body.title,
+                'image_url': body.image_url,
+                'subtitle': body.description,
+                'buttons': body.buttons
+              }
+            ]
+          }
+        }
+      }
+    }
+  } else if (body.componentType === 'gallery') {
+    var galleryCards = []
+    if (body.cards && body.cards.length > 0) {
+      for (var g = 0; g < body.cards.length; g++) {
+        var card = body.cards[g]
+        var galleryCard = {}
+        galleryCard.image_url = card.image_url
+        galleryCard.title = card.title
+        galleryCard.buttons = card.buttons
+        galleryCard.subtitle = card.subtitle
+        if (card.default_action) {
+          galleryCard.default_action = card.default_action
+        }
+        galleryCards.push(galleryCard)
+      }
+    }
+    payload = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'generic',
+          'elements': galleryCards
+        }
+      }
+    }
+  } else if (body.componentType === 'list') {
+    payload = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'list',
+          'top_element_style': body.topElementStyle,
+          'elements': body.listItems,
+          'buttons': body.buttons
+        }
+      }
+    }
+  } else if (body.componentType === 'media') {
+    payload = {
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'media',
+          'elements': [
+            {
+              'attachment_id': body.fileurl.attachment_id,
+              'media_type': body.mediaType,
+              'buttons': body.buttons
+            }
+          ]
+        }
+      }
+    }
+  }
+  return payload
+}
