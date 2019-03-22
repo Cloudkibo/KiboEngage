c4245455485cb0e614a865c57ccdfb1750fadc0a
Imran Shoukat <ishoukat@192.168.10.19>
2019-03-22 17:00:57 +0500

    modify create list

--- server/api/v1.1/lists/lists.controller.js
+++ server/api/v1.1/lists/lists.controller.js
@@ -4,6 +4,10 @@ const PollDataLayer = require('../polls/polls.datalayer')
 const SurveyDataLayer = require('../surveys/surveys.datalayer')
 const PollResponseDataLayer = require('../polls/pollresponse.datalayer')
 const SurveyResponseDataLayer = require('../surveys/surveyresponse.datalayer')
+const async = require('async')
+const logger = require('../../../components/logger')
+const TAG = 'api/lists/lists.controller.js'
+const { facebookApiCaller } = require('../../global/facebookApiCaller')
 
 exports.allLists = function (req, res) {
   utility.callApi(`companyUser/query`, 'post', { domain_email: req.user.domain_email }, req.headers.authorization)
@@ -67,75 +71,116 @@ exports.getAll = function (req, res) {
       })
     })
 }
-exports.createList = function (req, res) {
-  utility.callApi(`companyUser/query`, 'post', { domain_email: req.user.domain_email, populate: 'companyId' }, req.headers.authorization)
-    .then(companyUser => {
-      utility.callApi(`featureUsage/planQuery`, 'post', {planId: companyUser.companyId.planId}, req.headers.authorization)
-        .then(planUsage => {
-          planUsage = planUsage[0]
-          utility.callApi(`featureUsage/companyQuery`, 'post', {companyId: companyUser.companyId._id}, req.headers.authorization)
-            .then(companyUsage => {
-              companyUsage = companyUsage[0]
-              // add paid plan check later
-              // if (planUsage.segmentation_lists !== -1 && companyUsage.segmentation_lists >= planUsage.segmentation_lists) {
-              //   return res.status(500).json({
-              //     status: 'failed',
-              //     description: `Your lists limit has reached. Please upgrade your plan to premium in order to create more lists.`
-              //   })
-              // }
-              utility.callApi(`lists`, 'post', {
-                companyId: companyUser.companyId._id,
-                userId: req.user._id,
-                listName: req.body.listName,
-                conditions: req.body.conditions,
-                content: req.body.content,
-                parentList: req.body.parentListId,
-                parentListName: req.body.parentListName
-              }, req.headers.authorization)
-                .then(listCreated => {
-                  utility.callApi(`featureUsage/updateCompany`, 'put', {
-                    query: {companyId: req.body.companyId},
-                    newPayload: { $inc: { segmentation_lists: 1 } },
-                    options: {}
-                  }, req.headers.authorization)
-                    .then(updated => {
-                    })
-                    .catch(error => {
-                      return res.status(500).json({
-                        status: 'failed',
-                        payload: `Failed to update company usage ${JSON.stringify(error)}`
-                      })
-                    })
-                  return res.status(201).json({status: 'success', payload: listCreated})
-                })
-                .catch(error => {
-                  return res.status(500).json({
-                    status: 'failed',
-                    payload: `Failed to create list ${JSON.stringify(error)}`
-                  })
-                })
-            })
-            .catch(error => {
+exports.create = function (req, res) {
+  utility.callApi(`featureUsage/planQuery`, 'post', {planId: req.user.currentPlan._id}, req.headers.authorization)
+    .then(planUsage => {
+      planUsage = planUsage[0]
+      utility.callApi(`featureUsage/companyQuery`, 'post', {companyId: req.user.companyId}, req.headers.authorization)
+        .then(companyUsage => {
+          companyUsage = companyUsage[0]
+          // add paid plan check later
+          // if (planUsage.segmentation_lists !== -1 && companyUsage.segmentation_lists >= planUsage.segmentation_lists) {
+          //   return res.status(500).json({
+          //     status: 'failed',
+          //     description: `Your lists limit has reached. Please upgrade your plan to premium in order to create more lists.`
+          //   })
+          // }
+          async.parallelLimit([
+            function (callback) {
+              createList(req, callback)
+            },
+            function (callback) {
+              createTag(req, callback)
+            }
+          ], 10, function (err, results) {
+            if (err) {
               return res.status(500).json({
                 status: 'failed',
-                payload: `Failed to fetch company usage ${JSON.stringify(error)}`
+                payload: `Failed to create list ${JSON.stringify(err)}`
               })
-            })
+            }
+            assignTagToSubscribers(req.body.content, req.body.listName, req, res)
+          })
         })
         .catch(error => {
           return res.status(500).json({
             status: 'failed',
-            payload: `Failed to plan usage ${JSON.stringify(error)}`
+            payload: `Failed to fetch company usage ${JSON.stringify(error)}`
           })
         })
     })
     .catch(error => {
       return res.status(500).json({
         status: 'failed',
-        payload: `Failed to company profile ${JSON.stringify(error)}`
+        payload: `Failed to plan usage ${JSON.stringify(error)}`
       })
     })
 }
+
+function createTag (req, callback) {
+  utility.callApi('pages/query', 'post', {companyId: req.user.companyId, connected: true}, req.headers.authorization)
+    .then(pages => {
+      pages.forEach((page, i) => {
+        facebookApiCaller('v2.11', `me/custom_labels?access_token=${page.accessToken}`, 'post', {'name': req.body.tag})
+          .then(label => {
+            if (label.body.error) {
+              callback(label.body.error)
+            }
+            let tagPayload = {
+              tag: req.body.listName,
+              userId: req.user._id,
+              companyId: req.user.companyId,
+              pageId: page._id,
+              labelFbId: label.body.id,
+              isList: true
+            }
+            utility.callApi('tags/', 'post', tagPayload, req.headers.authorization)
+              .then(newTag => {
+                utility.callApi('featureUsage/updateCompany', 'put', {query: {companyId: req.user.companyId}, newPayload: { $inc: { labels: 1 } }, options: {}}, req.headers.authorization)
+                  .then(updated => {
+                    logger.serverLog(TAG, `Updated Feature Usage ${JSON.stringify(updated)}`)
+                  })
+                  .catch(err => {
+                    if (err) {
+                      logger.serverLog(TAG, `ERROR in updating Feature Usage${JSON.stringify(err)}`)
+                    }
+                  })
+                if (i === pages.length - 1) {
+                  callback(null, newTag)
+                }
+              })
+              .catch(err => callback(err))
+          })
+          .catch(err => callback(err))
+      })
+    })
+    .catch(err => callback(err))
+}
+
+function createList (req, callback) {
+  utility.callApi(`lists`, 'post', {
+    companyId: req.user.companyId,
+    userId: req.user._id,
+    listName: req.body.listName,
+    conditions: req.body.conditions,
+    content: req.body.content,
+    parentList: req.body.parentListId,
+    parentListName: req.body.parentListName
+  }, req.headers.authorization)
+    .then(listCreated => {
+      utility.callApi(`featureUsage/updateCompany`, 'put', {
+        query: {companyId: req.body.companyId},
+        newPayload: { $inc: { segmentation_lists: 1 } },
+        options: {}
+      }, req.headers.authorization)
+        .then(updated => {
+          callback(null, 'success')
+        })
+        .catch(error => callback(error))
+    })
+    .catch(error => callback(error))
+}
+
 exports.editList = function (req, res) {
   utility.callApi(`lists/${req.body._id}`, 'put', {
     listName: req.body.listName,
@@ -349,4 +394,84 @@ exports.repliedSurveySubscribers = function (req, res) {
         payload: `Failed to fetch company user ${JSON.stringify(error)}`
       })
     })
+}
+
+function isTagExists (pageId, tags) {
+  let temp = tags.map((t) => t.pageId)
+  let index = temp.indexOf(pageId)
+  if (index > -1) {
+    return {status: true, index}
+  } else {
+    return {status: false}
+  }
+}
+
+function assignTagToSubscribers (subscribers, tag, req, res) {
+  let tags = []
+  subscribers.forEach((subscriberId, i) => {
+    utility.callApi(`subscribers/${subscriberId}`, 'get', {}, req.headers.authorization)
+      .then(subscriber => {
+        let existsTag = isTagExists(subscriber.pageId._id, tags)
+        if (existsTag.status) {
+          let tagPayload = tags[existsTag.index]
+          facebookApiCaller('v2.11', `me/${tagPayload.labelFbId}/label?access_token=${subscriber.pageId.accessToken}`, 'post', {'user': subscriber.senderId})
+            .then(assignedLabel => {
+              if (assignedLabel.body.error) {
+                res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${assignedLabel.body.error}`})
+              }
+              let subscriberTagsPayload = {
+                tagId: tagPayload._id,
+                subscriberId: subscriber._id,
+                companyId: req.user.companyId
+              }
+              utility.callApi(`tags_subscriber/`, 'post', subscriberTagsPayload, req.headers.authorization)
+                .then(newRecord => {
+                  if (i === subscribers.length - 1) {
+                    res.status(200).json({status: 'success', payload: 'List created successfully!'})
+                  }
+                })
+                .catch(err => {
+                  res.status(500).json({status: 'failed', payload: `Failed to assign tag to subscriber ${err}`})
+                })
+            })
+            .catch(err => {
+              res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${err}`})
+            })
+        } else {
+          utility.callApi('tags/query', 'post', {tag, pageId: subscriber.pageId._id}, req.headers.authorization)
+            .then(tagPayload => {
+              tags.push(tagPayload)
+              facebookApiCaller('v2.11', `me/${tagPayload.labelFbId}/label?access_token=${subscriber.pageId.accessToken}`, 'post', {'user': subscriber.senderId})
+                .then(assignedLabel => {
+                  if (assignedLabel.body.error) {
+                    res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${assignedLabel.body.error}`})
+                  }
+                  let subscriberTagsPayload = {
+                    tagId: tagPayload._id,
+                    subscriberId: subscriber._id,
+                    companyId: req.user.companyId
+                  }
+                  utility.callApi(`tags_subscriber/`, 'post', subscriberTagsPayload, req.headers.authorization)
+                    .then(newRecord => {
+                      if (i === subscribers.length - 1) {
+                        res.status(200).json({status: 'success', payload: 'List created successfully!'})
+                      }
+                    })
+                    .catch(err => {
+                      res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${err}`})
+                    })
+                })
+                .catch(err => {
+                  res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${err}`})
+                })
+            })
+            .catch(err => {
+              res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${err}`})
+            })
+        }
+      })
+      .catch(err => {
+        res.status(500).json({status: 'failed', payload: `Failed to associate tag to subscriber ${err}`})
+      })
+  })
 }
