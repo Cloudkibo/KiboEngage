1a6558cabc80627065a6e6006bd1d3a007310fdd
Faizan Rasheed <Faizanrasheed4@gmail.com>
2019-03-28 13:50:14 +0500

    Update index.js

--- server/api/v1.1/utility/index.js
+++ server/api/v1.1/utility/index.js
@@ -26,9 +26,9 @@ exports.callApi = (endpoint, method = 'get', body, token, type = 'accounts') => 
     body,
     json: true
   }
-  // logger.serverLog(TAG, `requestPromise body ${util.inspect(body)}`)
+  logger.serverLog(TAG, `requestPromise body ${util.inspect(body)}`)
   return requestPromise(options).then(response => {
-    // logger.serverLog(TAG, `response from accounts ${util.inspect(response)}`)
+    logger.serverLog(TAG, `response from accounts ${util.inspect(response)}`)
     return new Promise((resolve, reject) => {
       if (response.status === 'success') {
         resolve(response.payload)
