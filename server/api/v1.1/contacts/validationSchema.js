b602479522b6462697add46aa01def26abca5769
anishachhatwani <i130315@nu.edu.pk>
2019-04-08 14:13:50 +0500

    kibodash work

--- server/api/v1.1/contacts/validationSchema.js
+++ server/api/v1.1/contacts/validationSchema.js
@@ -1,3 +1,25 @@
+exports.uploadPayload = {
+  type: 'object',
+  properties: {
+    phoneColumn: {
+      type: 'string',
+      required: true
+    },
+    nameColumn: {
+      type: 'string',
+      required: true
+    }
+  }
+}
+exports.uploadNumbersPayload = {
+  type: 'object',
+  properties: {
+    numbers: {
+      type: 'array',
+      required: true
+    }
+  }
+}
 exports.payload = {
   type: 'object',
   properties: {
