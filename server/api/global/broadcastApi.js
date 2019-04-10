d661ffc906e9b6d0d65408991ed8138a3c0e192e
Imran Shoukat <ishoukat@192.168.0.108>
2019-04-10 11:38:45 +0500

    promise

--- server/api/global/broadcastApi.js
+++ server/api/global/broadcastApi.js
@@ -2,51 +2,47 @@ const prepareMessageData = require('./prepareMessageData')
 const { facebookApiCaller } = require('./facebookApiCaller')
 
 exports.callBroadcastMessagesEndpoint = (messageCreativeId, labels, pageAccessToken) => {
-  let data = {
-    'message_creative_id': messageCreativeId,
-    'notification_type': 'REGULAR',
-    'messaging_type': 'MESSAGE_TAG',
-    'tag': 'NON_PROMOTIONAL_SUBSCRIPTION',
-    'targetting': {
-      'labels': {
-        'operator': 'AND',
-        'values': labels
+  return new Promise((resolve, reject) => {
+    let data = {
+      'message_creative_id': messageCreativeId,
+      'notification_type': 'REGULAR',
+      'messaging_type': 'MESSAGE_TAG',
+      'tag': 'NON_PROMOTIONAL_SUBSCRIPTION',
+      'targetting': {
+        'labels': {
+          'operator': 'AND',
+          'values': labels
+        }
       }
     }
-  }
-  facebookApiCaller('v2.11', `me/broadcast_messages?access_token=${pageAccessToken}`, 'post', data)
-    .then(response => {
-      return new Promise((resolve, reject) => {
+    facebookApiCaller('v2.11', `me/broadcast_messages?access_token=${pageAccessToken}`, 'post', data)
+      .then(response => {
         if (response.broadcast_id) {
           resolve({status: 'success', broadcast_id: response.broadcast_id})
         } else {
           resolve({status: 'failed', description: response.error})
         }
       })
-    })
-    .catch(err => {
-      return new Promise((resolve, reject) => {
+      .catch(err => {
         reject(err)
       })
-    })
+  })
 }
 
 exports.callMessageCreativesEndpoint = (data, pageAccessToken) => {
-  facebookApiCaller('v2.11', `me/message_creatives?access_token=${pageAccessToken}`, 'post', data)
-    .then(response => {
-      return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    facebookApiCaller('v2.11', `me/message_creatives?access_token=${pageAccessToken}`, 'post', data)
+      .then(response => {
         if (response.message_creative_id) {
           resolve({status: 'success', message_creative_id: response.message_creative_id})
         } else {
           resolve({status: 'failed', description: response.error})
         }
       })
-    })
-    .catch(err => {
-      return new Promise((resolve, reject) => {
+      .catch(err => {
         reject(err)
       })
-    })
+  })
 }
 
 exports.getMessagesData = (payload) => {
