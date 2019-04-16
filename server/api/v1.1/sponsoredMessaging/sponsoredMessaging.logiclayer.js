83cfec4bc49af5eb49d44f1b8805ef3236515c37
Muzamil Bukhari <muzamil.bukhari.39@gmail.com>
2019-04-16 14:22:25 +0500

    Created Sponsored message update endpoint

--- server/api/v1.1/sponsoredMessaging/sponsoredMessaging.logiclayer.js
+++ server/api/v1.1/sponsoredMessaging/sponsoredMessaging.logiclayer.js
@@ -6,3 +6,19 @@ exports.preparePayload = function(companyId,userId){
 
     return payload
 }
+
+exports.prepareUpdatePayload = function(body){
+    let payload={
+        _id: body._id,
+        payload: body.payload,
+        message_creative_id: body.message_creative_id,
+        ad_set_payload: body.ad_set_payload,
+        ad_id: body.ad_id,
+        campaign_name: body.campaign_name,
+        campaign_id: body.campaign_id,
+        status: body.status,
+        pageId: body.pageId,
+        statsFromUs: body.statsFromUs
+    }
+    return payload
+}
