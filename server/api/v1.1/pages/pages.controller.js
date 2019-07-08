9b15053e25c7b9d2a63dae4379c6b1898911e371
anishachhatwani <i130315@nu.edu.pk>
2019-07-08 14:43:33 +0500

    adding logs

--- server/api/v1.1/pages/pages.controller.js
+++ server/api/v1.1/pages/pages.controller.js
@@ -270,11 +270,7 @@ exports.enable = function (req, res) {
                                                 }
                                                 let bodyToSend = {
                                                   subscribed_fields: [
-                                                    'feed', 'mention', 'name', 'picture', 'category', 'description', 'conversations', 'feature_access_list',
-                                                    'messages', 'messaging_account_linking', 'messaging_checkout_updates',
-                                                    'message_echoes', 'message_deliveries', 'messaging_optins',
-                                                    'messaging_optouts', 'messaging_payments', 'messaging_postbacks', 'message_reads', 'messaging_referrals', 'messaging_policy_enforcement',
-                                                    'messaging_page_feedback', 'messaging_appointments', 'founded', 'general_info', 'leadgen', 'phone', 'email', 'website', 'product_review', 'videos', 'live_videos', 'registration']
+                                                    'feed']
                                                 }
                                                 needle.post(`https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=${page.accessToken}`, bodyToSend, (error, response) => {
                                                   console.log('response.body', response.body)
