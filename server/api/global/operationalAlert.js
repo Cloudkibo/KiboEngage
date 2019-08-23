72c085db5b7e81e223e7d1c0406a2cc18bcc6569
Sojharo <sojharo@live.com>
2019-08-23 15:38:48 +0500

    error on alert

--- server/api/global/operationalAlert.js
+++ server/api/global/operationalAlert.js
@@ -5,9 +5,9 @@ let TAG = 'server/api/global/operationalAlert.js'
 
 exports.sendOpAlert = function (errObj, codePart) {
   let email = getEmailObject('sojharo@cloudkibo.com', 'support@cloudkibo.com', 'KiboPush: Facebook Error', 'Facebook Error')
-  let emailWithBody = getAlertEmailWithBody(email, errObj.message, codePart)
+  // let emailWithBody = getAlertEmailWithBody(email, errObj.message, codePart)
   getSendGridObject()
-    .send(emailWithBody, function (err, json) {
+    .send(email, function (err, json) {
       if (err) {
         logger.log(TAG, 'error in sending Alert email ' + err, 'debug')
       }
