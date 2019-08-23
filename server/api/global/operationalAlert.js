5b4640fd20485ceda6f25cf27a50a3324fb69a02
Sojharo <sojharo@live.com>
2019-08-23 15:58:15 +0500

    error on alert

--- server/api/global/operationalAlert.js
+++ server/api/global/operationalAlert.js
@@ -6,10 +6,12 @@ let TAG = 'server/api/global/operationalAlert.js'
 exports.sendOpAlert = function (errObj, codePart) {
   let email = getEmailObject('sojharo@cloudkibo.com', 'support@cloudkibo.com', 'KiboPush: Facebook Error', 'Facebook Error')
   // let emailWithBody = getAlertEmailWithBody(email, errObj.message, codePart)
-  getSendGridObject()
+  if (require('./../../config/environment').env === 'production') {
+    getSendGridObject()
     .send(email, function (err, json) {
       if (err) {
         logger.log(TAG, 'error in sending Alert email ' + err, 'debug')
       }
     })
+  }
 }
