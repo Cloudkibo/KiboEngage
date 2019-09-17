36629d11e5650f133698ad474c4c3069b2787673
Sojharo <sojharo@live.com>
2019-09-17 14:36:37 +0500

    work on fb alert error

--- server/api/global/broadcastApi.js
+++ server/api/global/broadcastApi.js
@@ -25,8 +25,13 @@ exports.callBroadcastMessagesEndpoint = (messageCreativeId, labels, notlabels, p
         if (response.body.broadcast_id) {
           resolve({status: 'success', broadcast_id: response.body.broadcast_id})
         } else {
-          sendOpAlert(response.body.error, 'in broadcast api.js', page._id, page.userId, page.companyId)
-          resolve({status: 'failed', description: response.body.error})
+          let errorObj = response.body.error
+          if (errorObj && errorObj.code === 10 && errorObj.error_subcode === 2018065) {
+            resolve({status: 'failed', description: errorObj.message})
+          } else {
+            sendOpAlert(response.body.error, 'in broadcast api.js', page._id, page.userId, page.companyId)
+            resolve({status: 'failed', description: response.body.error})
+          }
         }
       })
       .catch(err => {
