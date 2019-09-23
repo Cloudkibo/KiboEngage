4aecb8ff513bcc4369dc40f14b1c24ed9dd713ca
Baqar <bjafri5@gmail.com>
2019-09-23 16:44:57 +0500

    log

--- server/api/v1.1/URLForClickedCount/URL.controller.js
+++ server/api/v1.1/URLForClickedCount/URL.controller.js
@@ -29,6 +29,7 @@ exports.broadcast = function (req, res) {
   URLDataLayer.findOneURL(req.params.id)
     .then(URLObject => {
       if (URLObject) {
+        logger.serverLog(TAG, `URLObject found, incrementing click ${JSON.stringify(URLObject)}`, 'debug')
         BroadcastsDataLayer.updateBroadcast({_id: URLObject.module.id}, {$inc: {clicks: 1}})
           .then(updatedData => {
             res.writeHead(301, {Location: URLObject.originalURL.startsWith('http') ? URLObject.originalURL : `https://${URLObject.originalURL}`})
