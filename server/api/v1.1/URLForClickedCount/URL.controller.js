917e667a20104602e54e9c62ed127ea939518ebf
Baqar <bjafri5@gmail.com>
2019-09-25 12:30:19 +0500

    debug log

--- server/api/v1.1/URLForClickedCount/URL.controller.js
+++ server/api/v1.1/URLForClickedCount/URL.controller.js
@@ -27,7 +27,7 @@ exports.index = function (req, res) {
 
 exports.broadcast = function (req, res) {
   // logger.serverLog(TAG, `broadcast click count increased ${req.params.id}`, 'debug')
-  logger.serverLog(TAG, `broadcast click count increased ${util.inspect(req)}`)
+  logger.serverLog(TAG, `broadcast click count increased ${util.inspect(req)}`, 'debug')
   URLDataLayer.findOneURL(req.params.id)
     .then(URLObject => {
       if (URLObject) {
