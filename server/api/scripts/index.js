da9853a1e856e814a34eb57a77fe81f39903838a
ImranBinShoukat <imran.shoukat1995@gmail.com>
2019-10-02 15:42:10 +0500

    common function for batch api

--- server/api/scripts/index.js
+++ server/api/scripts/index.js
@@ -5,5 +5,6 @@ const controller = require('./controller')
 router.get('/normalizeDataForDelivery', controller.normalizeDataForDelivery)
 router.get('/addWhitelistDomain', controller.addWhitelistDomain)
 router.post('/performanceTest/broadcast', controller.performanceTestBroadcast)
+router.post('/testCommonBatchAPI', controller.testCommonBatchAPI)
 
 module.exports = router
