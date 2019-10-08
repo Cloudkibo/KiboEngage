877abc16708d32fd1f4df16a7a8dc812baf575d9
anishachhatwani <i130315@nu.edu.pk>
2019-10-08 13:24:44 +0500

    whatsAppChat

--- server/api/v1.1/whatsAppBroadcasts/whatsAppBroadcasts.logiclayer.test.js
+++ server/api/v1.1/whatsAppBroadcasts/whatsAppBroadcasts.logiclayer.test.js
@@ -1,4 +1,4 @@
-const { prepareChat } = require('./utility.js')
+const { prepareChat } = require('./whatsAppBroadcasts.logiclayer.js')
 
 describe('Validate prepareChat', () => {
   test('should return an object', () => {
@@ -20,24 +20,5 @@ describe('Validate prepareChat', () => {
       payload: {componentType: 'text', text: 'hi'}
     }
     expect(prepareChat(payload, companyUser, contact)).toEqual(output)
-  })
-  test('should throw error', () => {
-    let payload = [{componentType: 'text', text: 'hi'}]
-    let companyUser = {
-      companyId: {
-        twilioWhatsApp: {sandboxNumber: '+123'}}
-    }
-    let contact = {
-      _id: '5abc',
-      number: '+923322'
-    }
-    let output = {
-      senderNumber: '+123',
-      recipientNumber: '+923322',
-      contactId: '5abc',
-      companyId: '123',
-      payload: {componentType: 'text', text: 'hi'}
-    }
-    expect(prepareChat(payload, companyUser, contact)).toThrow(TypeError)
   })
 })
