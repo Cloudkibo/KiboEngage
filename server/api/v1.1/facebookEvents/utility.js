c69ffd15b11249093d4ad1758cb49f9f38b9d7e9
Sojharo <sojharo@live.com>
2019-10-14 16:38:51 +0500

    final work on autoposting type CloudKibo/KiboPush#6807

--- server/api/v1.1/facebookEvents/utility.js
+++ server/api/v1.1/facebookEvents/utility.js
@@ -63,7 +63,7 @@ exports.sentUsinInterval = function (messageData, page, postingItem, subscribers
                             utility.callApi('autoposting_messages', 'put', {purpose: 'updateOne', match: {_id: postingItem._id}, updated: {messageCreativeId, broadcastFbId: response.broadcast_id, APIName: 'broadcast_api'}}, 'kiboengage')
                               .then(updated => {
                                 require('../../global/messageStatistics').record('autoposting')
-                                let autopostingType = postingItem.autoposting_type
+                                let autopostingType = updated.autoposting_type
                                 require('../../global/messageStatistics').record(`autoposting${autopostingType}`)
                                 current++
                                 send = true
