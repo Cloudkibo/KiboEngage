6fa4bdd07ce9dd29545c3297993b4431a7cd6d8e
sania <sania@cloudkibo.com>
2019-11-21 11:19:47 +0500

    server work comment capture analytics

--- server/api/v1.1/commentCapture/commentCapture.logiclayer.js
+++ server/api/v1.1/commentCapture/commentCapture.logiclayer.js
@@ -3,7 +3,6 @@ This file will contain the functions for logic layer.
 By separating it from controller, we are separating the concerns.
 Thus we can use it from other non express callers like cron etc
 */
-const mongoose = require('mongoose')
 const URL = require('url')
 
 exports.setMessage = function (payload) {
