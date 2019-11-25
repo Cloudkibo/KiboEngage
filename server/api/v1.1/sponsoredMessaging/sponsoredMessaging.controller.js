350991ade624e2d977bd2fa107b00a706807b2bc
Cloudkibo-Faizan <faizanrasheed4@gmail.com>
2019-11-25 19:21:09 +0500

    removed console

--- server/api/v1.1/sponsoredMessaging/sponsoredMessaging.controller.js
+++ server/api/v1.1/sponsoredMessaging/sponsoredMessaging.controller.js
@@ -3,6 +3,7 @@ const logiclayer = require('./sponsoredMessaging.logiclayer')
 const { facebookApiCaller } = require('../../global/facebookApiCaller')
 const { marketingApiAccessToken } = require('../../../config/environment')
 let { sendOpAlert } = require('./../../global/operationalAlert')
+const logger = require('../../../components/logger')
 
 exports.index = function (req, res) {
   utility.callApi(`companyUser/query`, 'post', { domain_email: req.user.domain_email })
@@ -63,44 +64,44 @@ exports.update = function (req, res) {
 exports.send = function (req, res) {
   const accesstoken = marketingApiAccessToken
   let id = req.params.id
-  console.log('id', id)
+  logger.serverLog('id', id)
 
   if (id !== undefined && id !== '') {
     utility.callApi(`sponsoredMessaging/query`, 'get', { _id: id })
       .then(sponsoredMessages => {
         let sponsoredMessage = sponsoredMessages[0]
         let campaignPayload = logiclayer.prepareCampaignPayload(sponsoredMessage, accesstoken)
-        console.log('campaign paylaod', campaignPayload)
+        logger.serverLog('campaign paylaod', campaignPayload)
         facebookApiCaller('v4.0', `act_${req.body.ad_account_id}/campaigns`, 'post', campaignPayload)
           .then(resp => {
             if (resp.body.error) {
               sendOpAlert(resp.body.error, 'sponsored messaging controller in kiboengage', '', req.user._id, req.user.companyId)
             }
             let campaignId = resp.body.id
-            console.log('campaign id', resp.body)
+            logger.serverLog('campaign id', resp.body)
             let adsetPayload = logiclayer.prepareAdsetPayload(sponsoredMessage, campaignId, accesstoken)
-            console.log('adsetPayload', adsetPayload)
+            logger.serverLog('adsetPayload', adsetPayload)
             facebookApiCaller('v4.0', `act_${req.body.ad_account_id}/adsets`, 'post', adsetPayload)
               .then(response => {
                 if (response.body.error) {
                   sendOpAlert(response.body.error, 'sponsored messaging controller in kiboengage', '', req.user._id, req.user.companyId)
                 }
                 let adsetid = response.body.id
-                console.log('adset', adsetid)
+                logger.serverLog('adset', adsetid)
                 let creativePayload = logiclayer.prepareadCreativePayload(sponsoredMessage, accesstoken)
-                console.log('creativePayload', creativePayload)
+                logger.serverLog('creativePayload', creativePayload)
 
                 facebookApiCaller('v4.0', `act_${req.body.ad_account_id}/adcreatives`, 'post', creativePayload)
                   .then(resp => {
                     let messageCreativeId = resp.id
-                    console.log('messageCreativeId', messageCreativeId)
+                    logger.serverLog('messageCreativeId', messageCreativeId)
                     let adPayload = logiclayer.prepareadAdPayload(sponsoredMessage, adsetid, messageCreativeId, accesstoken)
-                    console.log('adPayload', adPayload)
+                    logger.serverLog('adPayload', adPayload)
 
                     facebookApiCaller('v4.0', `act_${req.body.ad_account_id}/ads`, 'post', adPayload)
                       .then(resp => {
                         let ad_id = resp.id
-                        console.log('ad_id',  ad_id)
+                        logger.serverLog('ad_id',  ad_id)
                         // Now since we have got respone from facebook, we shall update our database
                         let updatePayload = logiclayer.prepareUpdatePayload({ campaign_id: campaignId, ad_id: ad_id, ad_set_payload: { adset_id: adsetid }, messageCreativeId: messageCreativeId })
                         utility.callApi(`sponsoredMessaging/${req.params._id}`, 'post', updatePayload, req.headers.authorization)
