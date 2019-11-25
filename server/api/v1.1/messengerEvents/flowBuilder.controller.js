b7d48eca02ef8ed9789d160fcf00e4926442f943
Sojharo <sojharo@live.com>
2019-11-25 18:20:24 +0500

    flow builder server CloudKibo/KiboPush#7168
    flow builder server CloudKibo/KiboPush#7168

--- server/api/v1.1/messengerEvents/flowBuilder.controller.js
+++ server/api/v1.1/messengerEvents/flowBuilder.controller.js
@@ -3,30 +3,31 @@ const logger = require('../../../components/logger')
 const TAG = 'api/v1/messengerEvents/flowBuilder.controller.js'
 const BroadcastPageDataLayer = require('../page_broadcast/page_broadcast.datalayer')
 const { sendUsingBatchAPI } = require('../../global/sendConversation')
-
 exports.index = function (req, res) {
   res.status(200).json({
     status: 'success',
     description: `received the payload`
   })
-  const sender = req.body.senderId
-  const pageId = req.body.pageId
-  const blockUniqueId = req.body.entry[0].messaging[0].postback.payload.blockUniqueId
+  const sender = req.body.entry[0].messaging[0].sender.id
+  const pageId = req.body.entry[0].messaging[0].recipient.id
+  const parsedPayload = JSON.parse(req.body.entry[0].messaging[0].postback.payload)
+  const blockUniqueId = parsedPayload.blockUniqueId
   let reportObj = {
     successful: 0,
     unsuccessful: 0,
     errors: []
   }
+  console.log('WE CAME HERE')
   callApi(`pages/query`, 'post', { pageId: pageId, connected: true })
     .then(page => {
       page = page[0]
       let subsFindCriteria = { pageId: page._id, companyId: page.companyId, senderId: sender, completeInfo: true }
-      callApi(`messageBlocks/query`, 'post', { purpose: 'findOne', match: { uniqueId: blockUniqueId } }, 'kiboengage')
+      callApi(`messageBlocks/query`, 'post', { purpose: 'findOne', match: { uniqueId: '' + blockUniqueId } }, 'kiboengage')
         .then(messageBlock => {
           if (messageBlock.module.type === 'broadcast') {
             let pageBroadcastData = {
               pageId: page.pageId,
-              userId: page.userId,
+              userId: page.userId._id,
               broadcastId: messageBlock.module.id,
               seen: false,
               sent: false,
@@ -44,7 +45,6 @@ exports.index = function (req, res) {
       logger.serverLog(TAG, `Failed to fetch page ${JSON.stringify(err)}`, 'error')
     })
 }
-
 const _savePageBroadcast = (data) => {
   BroadcastPageDataLayer.createForBroadcastPage(data)
     .then(savedpagebroadcast => {
