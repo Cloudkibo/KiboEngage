3a32f0c745c02074927a461971a8aea0c3b4ed6a
Sojharo <sojharo@live.com>
2019-11-26 18:45:52 +0500

    final test offline sheets

--- server/api/v1.1/sheetsIntegration/sheetsIntegration.controller.js
+++ server/api/v1.1/sheetsIntegration/sheetsIntegration.controller.js
@@ -160,7 +160,7 @@ function populateGoogleColumns (dataToSend, googleData, sheetId) {
 exports.auth = function (req, res) {
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'online',
+    access_type: 'offline',
 
     // If you only need one scope you can pass it as a string
     scope: config.google.scopes
