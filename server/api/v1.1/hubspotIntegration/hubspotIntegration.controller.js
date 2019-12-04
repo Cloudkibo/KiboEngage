347d6ff908c95d75ae8319e36bd37ef066eeddc3
Sojharo <sojharo@live.com>
2019-12-04 16:29:58 +0500

    hubspot form api testing CloudKibo/KiboPush#7021

--- server/api/v1.1/hubspotIntegration/hubspotIntegration.controller.js
+++ server/api/v1.1/hubspotIntegration/hubspotIntegration.controller.js
@@ -183,7 +183,7 @@ function callHubspotApi (url, method, body, accessToken) {
         if (error) {
           reject(error)
         } else {
-          console.log(data)
+          console.log(data.body)
           if (data.statusCode && data.statusCode === 200) {
             resolve(JSON.parse(data.body))
           } else {
