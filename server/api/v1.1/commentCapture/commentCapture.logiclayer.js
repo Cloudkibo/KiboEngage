79641a247b4114a05b12e89210f81c622df4e819
sania <sania@cloudkibo.com>
2019-12-18 10:03:41 +0500

    Fixing Comments

--- server/api/v1.1/commentCapture/commentCapture.logiclayer.js
+++ server/api/v1.1/commentCapture/commentCapture.logiclayer.js
@@ -89,7 +89,7 @@ exports.getCountForComments = (body) => {
 
 exports.getComments = (body) => {
   let aggregateData = [
-    { $match: {postId: body.postId, parentId: {$exists: false}} },
+    { $match: {parentId: {$exists: false}} },
     { $sort: {_id: body.sort_value} },
     { $match: {
       '_id': body.first_page ? {$exists: true} : body.sort_value === -1 ? {$lt: body.last_id} : {$gt: body.last_id}
@@ -98,6 +98,8 @@ exports.getComments = (body) => {
   ]
   if (body.post_id && body.post_id !== '') {
     aggregateData[0].$match.postFbId = body.post_id
+  } else { 
+    aggregateData[0].$match.postId = body.postId
   }
   return aggregateData
 }
@@ -274,8 +276,9 @@ function handleLinks (textComponents, linkComponents) {
   } else if (linkComponents.length > 1) {
     let links = []
     for (let i = 0; i < linkComponents.length && i < 10; i++) {
-      links.push({'link': linkComponents[i].url})
+      links.push({'link': {'link':linkComponents[i].url, 'picture': linkComponents[i].card.image_url, 'name': linkComponents[i].card.title, 'description': linkComponents[i].card.subtitle}})
     }
+    console.log('Links', links)
     payload = {
       type: 'text',
       payload: {
