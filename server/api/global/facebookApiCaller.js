4fbf151b2486e996da9c2c3c128f722b7bcd3c44
saniasiddiqui <31842853+saniasiddiqui@users.noreply.github.com>
2019-12-20 14:15:06 +0500

    Test data

--- server/api/global/facebookApiCaller.js
+++ server/api/global/facebookApiCaller.js
@@ -2,6 +2,7 @@ const needle = require('needle')
 
 exports.facebookApiCaller = (version, path, method, data) => {
   console.log(`https://graph.facebook.com/${version}/${path}`)
+  console.log('Data', data)
   return needle(
     method.toLowerCase(),
     `https://graph.facebook.com/${version}/${path}`,
