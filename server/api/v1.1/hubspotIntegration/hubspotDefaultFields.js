eb931dc68c37a7e271a1db89082f366e90fad73d
arveen kumar <arveenkumar55@gmail.com>
2019-12-23 16:55:04 +0500

    reomve_columns

--- server/api/v1.1/hubspotIntegration/hubspotDefaultFields.js
+++ server/api/v1.1/hubspotIntegration/hubspotDefaultFields.js
@@ -1,12 +1,12 @@
 exports.defaultFieldcolumn = {
   hubSpotColumns: ['Annual Revenue', 'City', 'Close Date', 'Company Name', 'Company size', 'Contact owner', 'Country/Region', 'Date of birth', 'Degree', 'Email', 'Fax Number', 'Field of study', 'First Name', 'Gender', 'Graduation date', 'Industry', 'Job Title', 'Job function', 'Last Name', 'Lead Status', 'Legal basis for processing contacts data', 'Lifecycle Stage',
-    'Marital Status', 'Membership Notes', 'Message', 'Military status', 'Mobile Phone Number', 'Number of Employees', 'Original Source',
-    'Persona', 'Phone Number', 'Postal Code', 'Preferred language', 'Relationship Status', 'Salutation', 'School', 'Seniority', 'Start date'
+    'Marital Status', 'Membership Notes', 'Message', 'Military status', 'Mobile Phone Number', 'Number of Employees', 'Original Source'
+    //'Persona', 'Phone Number', 'Postal Code', 'Preferred language', 'Relationship Status', 'Salutation', 'School', 'Seniority', 'Start date'
     //State/Region', 'Status', 'Street Address', 'Twitter Username', 'Website URL', 'Work email'
   ],
   HubspotMappingColumns: ['annualrevenue', 'city', 'closedate', 'company', 'company_size', 'hubspot_owner_id', 'country', 'date_of_birth', 'degree', 'email', 'fax', 'field_of_study', 'firstname', 'gender', 'graduation_date', 'industry', 'jobtitle', 'job_function', 'lastname', 'hs_lead_status', 'hs_legal_basis', 'lifecyclestage',
-    'marital_status', 'hs_content_membership_notes', 'message', 'military_status', 'mobilephone', 'numemployees', 'hs_analytics_source',
-    'hs_persona', 'phone', 'zip', 'hs_language', 'relationship_status', 'salutation', 'school', 'seniority', 'start_date'
+    'marital_status', 'hs_content_membership_notes', 'message', 'military_status', 'mobilephone', 'numemployees', 'hs_analytics_source'
+    //'hs_persona', 'phone', 'zip', 'hs_language', 'relationship_status', 'salutation', 'school', 'seniority', 'start_date'
    // 'state', 'hs_content_membership_status', 'address', 'twitterhandle', 'website', 'work_email'
 ]
 }
