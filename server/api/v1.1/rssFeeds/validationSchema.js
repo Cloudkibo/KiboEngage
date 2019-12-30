e9376a43047b9f7966b0248ae4e651b25fc93a9d
anishachhatwani <i130315@nu.edu.pk>
2019-12-30 16:40:54 +0500

    rss feeds

--- server/api/v1.1/rssFeeds/validationSchema.js
+++ server/api/v1.1/rssFeeds/validationSchema.js
@@ -34,3 +34,42 @@ exports.createPayload = {
     'pageIds'
   ]
 }
+
+exports.getRssFeedPostsPayload = {
+  'type': 'object',
+  'properties': {
+    'feedId': {
+      'type': 'string'
+    },
+    'requested_page': {
+      'type': 'integer'
+    },
+    'last_id': {
+      'type': 'string'
+    },
+    'current_page': {
+      'type': 'integer'
+    },
+    'number_of_records': {
+      'type': 'integer'
+    },
+    'first_page': {
+      'type': 'string'
+    },
+    'page_value': {
+      'type': 'string'
+    },
+    'startDate': {
+      'type': 'string'
+    },
+    'endDate': {
+      'type': 'string'
+    }
+  },
+  'required': [
+    'feedId',
+    'last_id',
+    'number_of_records',
+    'first_page'
+  ]
+}
