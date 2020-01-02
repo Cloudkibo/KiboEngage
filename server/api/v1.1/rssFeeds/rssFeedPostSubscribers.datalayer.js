c0f19d5cd2f8cf084a1912bdacd509a8579445cd
anishachhatwani <i130315@nu.edu.pk>
2020-01-02 17:14:17 +0500

    rss feeds

---
+++ server/api/v1.1/rssFeeds/rssFeedPostSubscribers.datalayer.js
@@ -0,0 +1,57 @@
+/*
+This file will contain the functions for data layer.
+By separating it from controller, we are separating the concerns.
+Thus we can use it from other non express callers like cron etc
+*/
+const { callApi } = require('../utility')
+
+exports.genericFind = (queryObject) => {
+  let query = {
+    purpose: 'findAll',
+    match: queryObject
+  }
+  return callApi(`rssFeedPostSubscribers/query`, 'post', query, 'kiboengage')
+}
+exports.aggregate = (match, group, lookup, limit, sort, skip) => {
+  let query = {
+    purpose: 'aggregate',
+    match: match
+  }
+  if (group) query.group = group
+  if (lookup) query.lookup = lookup
+  if (limit) query.limit = limit
+  if (sort) query.sort = sort
+  if (skip) query.skip = skip
+
+  return callApi(`rssFeedPostSubscribers/query`, 'post', query, 'kiboengage')
+}
+exports.create = (payload) => {
+  return callApi(`rssFeedPostSubscribers`, 'post', payload, 'kiboengage')
+}
+exports.delete = (queryObject) => {
+  let query = {
+    purpose: 'deleteMany',
+    match: queryObject
+  }
+  return callApi(`rssFeedPostSubscribers`, 'delete', query, 'kiboengage')
+}
+
+exports.countDocuments = (filter) => {
+  let query = {
+    purpose: 'aggregate',
+    match: filter,
+    group: { _id: null, count: { $sum: 1 } }
+  }
+  return callApi(`rssFeedPostSubscribers/query`, 'post', query, 'kiboengage')
+}
+exports.genericUpdate = (queryObject, updated, options) => {
+  let query = {
+    purpose: 'updateAll',
+    match: queryObject,
+    updated: updated
+  }
+  if (options.upsert) query.upsert = options.upsert
+  if (options.new) query.new = options.new
+  if (options.multi) query.multi = options.multi
+  return callApi(`rssFeedPostSubscribers`, 'put', query, 'kiboengage')
+}
--- server/api/v1.1/rssFeeds/rssFeeds.logiclayer.js
+++ server/api/v1.1/rssFeeds/rssFeeds.logiclayer.js
@@ -1,55 +1,55 @@
 exports.fetchFeedsCriteria = function (body, companyId) {
-    let finalCriteria = {}
-    let countCriteria = {}
-    let recordsToSkip = 0
-    let findCriteria = {
-      companyId: companyId,
-      title: body.search_value !== '' ? { $regex: body.search_value, $options: 'i' } : { $exists: true }
-    }
-    if (body.status_value !== '') {
-      findCriteria['isActive'] = body.status_value === 'true' ? true : false
-    }
-    console.log('Number of records', body.number_of_records)
-    if (body.first_page === 'first') {
-      finalCriteria = [
-        { $match: findCriteria },
-        { $sort: { datetime: -1 } },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    } else if (body.first_page === 'next') {
-      recordsToSkip = Math.abs(((body.requested_page - 1) - (body.current_page))) * body.number_of_records
-      let finalFindCriteria = {}
-      Object.assign(finalFindCriteria, findCriteria)
-      finalFindCriteria._id = { $lt: body.last_id }
-      finalCriteria = [
-        { $match: finalFindCriteria },
-        { $sort: { datetime: -1 } },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    } else if (body.first_page === 'previous') {
-      recordsToSkip = Math.abs(body.requested_page * body.number_of_records)
-      let finalFindCriteria = {}
-      Object.assign(finalFindCriteria, findCriteria)
-      finalFindCriteria._id = { $gt: body.last_id }
-      finalCriteria = [
-        { $match: finalFindCriteria },
-        { $sort: { datetime: -1 } },
-        { $skip: recordsToSkip },
-        { $limit: body.number_of_records }
-      ]
-    }
-    countCriteria = [
-      { $match: findCriteria },
-      { $group: { _id: null, count: { $sum: 1 } } }
-    ]
-    return {
-      finalCriteria,
-      countCriteria
-    }
+  let finalCriteria = {}
+  let countCriteria = {}
+  let recordsToSkip = 0
+  let findCriteria = {
+    companyId: companyId,
+    title: body.search_value !== '' ? { $regex: body.search_value, $options: 'i' } : { $exists: true }
   }
-  
+  if (body.status_value !== '') {
+    findCriteria['isActive'] = body.status_value === 'true' ? true : false
+  }
+  console.log('Number of records', body.number_of_records)
+  if (body.first_page === 'first') {
+    finalCriteria = [
+      { $match: findCriteria },
+      { $sort: { datetime: -1 } },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
+    ]
+  } else if (body.first_page === 'next') {
+    recordsToSkip = Math.abs(((body.requested_page - 1) - (body.current_page))) * body.number_of_records
+    let finalFindCriteria = {}
+    Object.assign(finalFindCriteria, findCriteria)
+    finalFindCriteria._id = { $lt: body.last_id }
+    finalCriteria = [
+      { $match: finalFindCriteria },
+      { $sort: { datetime: -1 } },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
+    ]
+  } else if (body.first_page === 'previous') {
+    recordsToSkip = Math.abs(body.requested_page * body.number_of_records)
+    let finalFindCriteria = {}
+    Object.assign(finalFindCriteria, findCriteria)
+    finalFindCriteria._id = { $gt: body.last_id }
+    finalCriteria = [
+      { $match: finalFindCriteria },
+      { $sort: { datetime: -1 } },
+      { $skip: recordsToSkip },
+      { $limit: body.number_of_records }
+    ]
+  }
+  countCriteria = [
+    { $match: findCriteria },
+    { $group: { _id: null, count: { $sum: 1 } } }
+  ]
+  return {
+    finalCriteria,
+    countCriteria
+  }
+}
+
 exports.getCriterias = function (body) {
   let startDate = new Date(body.startDate) // Current date
   startDate.setHours(0) // Set the hour, minute and second components to 0
