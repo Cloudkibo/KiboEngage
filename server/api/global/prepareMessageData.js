02d272a40290777f817de8318bd7cd4a3fa9b0df
arveen kumar <arveenkumar55@gmail.com>
2020-01-29 11:38:55 +0500

    fix_in_userInput

--- server/api/global/prepareMessageData.js
+++ server/api/global/prepareMessageData.js
@@ -12,20 +12,21 @@ exports.facebook = (body, fname, lname) => {
       attachment: body.attachment
     }
   } else if (body.componentType === 'userInput') {
+    let question = body.question
     if (body.question.includes('{{user_full_name}}') || body.question.includes('[Username]')) {
-      body.question = body.question.replace(
+      question = body.question.replace(
         '{{user_full_name}}', fname + ' ' + lname)
     }
     if (body.question.includes('{{user_first_name}}')) {
-      body.question = body.question.replace(
+      question = body.question.replace(
         '{{user_first_name}}', fname)
     }
     if (body.question.includes('{{user_last_name}}')) {
-      body.question = body.question.replace(
+      question = body.question.replace(
         '{{user_last_name}}', lname)
     }
     payload = {
-      'text': body.question
+      'text': question
     }
     if (body.quickReplies && body.quickReplies.length > 0) {
       payload.quick_replies = body.quickReplies
