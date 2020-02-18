576dd82f835d8b1573c93b55beba4e9f4d48cef6
sania <sania@cloudkibo.com>
2020-02-18 14:11:18 +0500

    Fix for connect issue in google sheets

--- server/api/v1.1/sheetsIntegration/sheetsIntegration.controller.js
+++ server/api/v1.1/sheetsIntegration/sheetsIntegration.controller.js
@@ -151,7 +151,7 @@ exports.auth = function (req, res) {
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
     access_type: 'offline',
-
+    prompt: 'consent',
     // If you only need one scope you can pass it as a string
     scope: config.google.scopes
   })
