4906eceeca63823d1d7f8f034581f1ff547c2b77
arveen kumar <arveenkumar55@gmail.com>
2020-03-17 17:32:03 +0500

    hot_fix

--- server/api/v1.1/smsBroadcasts/smsBroadcasts.logiclayer.js
+++ server/api/v1.1/smsBroadcasts/smsBroadcasts.logiclayer.js
@@ -116,14 +116,14 @@ const _checkFilterCondition = (values, matchCriteria) => {
       } else {
         matchCriteria[`${filter.condition}`] = {$regex: `^${filter.text}$`, $options: 'i'}
       }
-    } else if (filter.criteria === 'contains' && filter.text.includes('+')) {
-      if (filter.condition === 'number') {
+    } else if (filter.criteria === 'contains') {
+      if (filter.condition === 'number' && filter.text.includes('+')) {
         matchCriteria[`${filter.condition}`] = {$regex: `.*\\${filter.text}.*`, $options: 'i'}
       } else {
         matchCriteria[`${filter.condition}`] = {$regex: `.*${filter.text}.*`, $options: 'i'}
       }
-    } else if (filter.criteria === 'begins' && filter.text.includes('+')) {
-      if (filter.condition === 'number') {
+    } else if (filter.criteria === 'begins') {
+      if (filter.condition === 'number' && filter.text.includes('+')) {
         matchCriteria[`${filter.condition}`] = {$regex: `^\\${filter.text}`, $options: 'i'}
       } else {
         matchCriteria[`${filter.condition}`] = {$regex: `^${filter.text}`, $options: 'i'}
@@ -133,5 +133,4 @@ const _checkFilterCondition = (values, matchCriteria) => {
   console.log('matchCriteria', matchCriteria)
   return matchCriteria
 }
-
 exports.prepareBroadCastPayload = prepareBroadCastPayload
