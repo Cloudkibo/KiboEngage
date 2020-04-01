5a9dbfb19ed0518a6783c826ce70fa855cd00931
anishachhatwani <i130315@nu.edu.pk>
2020-04-01 13:01:42 +0500

    schedule

--- scripts/scheduleSponsored.js
+++ scripts/scheduleSponsored.js
@@ -1,9 +1,9 @@
-const utility = require('../utility')
-const logiclayer = require('./../server/api/v1.1/sponsoredMessaging/sponsoredMessaging.logiclayer')
-const datalayer = require('./../server/api/v1.1/sponsoredMessaging/sponsoredMessaging.datalayer')
-const async = require('async')
-const { facebookApiCaller } = require('../../global/facebookApiCaller')
-
-exports.runScheduleSponsored = () => {
-
-}
+// const utility = require('../utility')
+// const logiclayer = require('./../server/api/v1.1/sponsoredMessaging/sponsoredMessaging.logiclayer')
+// const datalayer = require('./../server/api/v1.1/sponsoredMessaging/sponsoredMessaging.datalayer')
+// const async = require('async')
+// const { facebookApiCaller } = require('../../global/facebookApiCaller')
+//
+// exports.runScheduleSponsored = () => {
+//
+// }
