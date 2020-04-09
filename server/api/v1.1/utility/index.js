d213a5bf7c11b761c07edf2b4201f12ee43a860b
Sojharo <sojharo@live.com>
2020-04-09 17:53:38 +0500

    hotfix http library

--- server/api/v1.1/utility/index.js
+++ server/api/v1.1/utility/index.js
@@ -1,17 +1,19 @@
-const requestPromise = require('request-promise')
+const needle = require('needle')
 const config = require('../../../config/environment/index')
+const logger = require('../../../components/logger')
+const TAG = 'api/v1.1/utility/index.js'
 const { accounts } = require('../../global/constants').serverConstants
 
 exports.callApi = (endpoint, method = 'get', body, type = accounts, token) => {
   let headers
   if (token && token !== '') {
     headers = {
-      'content-type': 'application/json',
+      'content_type': 'application/json',
       'Authorization': token
     }
   } else {
     headers = {
-      'content-type': 'application/json',
+      'content_type': 'application/json',
       'is_kibo_product': true
     }
   }
@@ -26,13 +28,18 @@ exports.callApi = (endpoint, method = 'get', body, type = accounts, token) => {
     body,
     json: true
   }
-  return requestPromise(options).then(response => {
-    return new Promise((resolve, reject) => {
-      if (response.status === 'success') {
-        resolve(response.payload)
-      } else {
-        reject(response.payload)
-      }
-    })
+  return new Promise((resolve, reject) => {
+    needle(method, options.uri, body, options)
+      .then(response => {
+        if (response.body.status === 'success') {
+          resolve(response.body.payload)
+        } else {
+          reject(response.body.payload)
+        }
+      })
+      .catch(error => {
+        reject(error)
+        logger.serverLog(TAG, `error in calling internal APIs ${JSON.stringify(error)}`, 'error')
+      })
   })
 }
