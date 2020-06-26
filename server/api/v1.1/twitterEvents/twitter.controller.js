da0c016c6818cc367b12176552c22b16da4f5aaa
anishachhatwani <i130315@nu.edu.pk>
2020-06-26 11:36:37 +0500

    adding logs

--- server/api/v1.1/twitterEvents/twitter.controller.js
+++ server/api/v1.1/twitterEvents/twitter.controller.js
@@ -33,7 +33,7 @@ exports.twitterwebhook = function (req, res) {
     status: 'success',
     description: `received the payload`
   })
-  console.log('twitter event got', req.body)
+  console.log('twitter event got', JSON.stringify(req.body))
   AutoPosting.findAllAutopostingObjectsUsingQuery({accountUniqueName: req.body.user.screen_name, isActive: true})
     .then(autopostings => {
       autopostings.forEach(postingItem => {
