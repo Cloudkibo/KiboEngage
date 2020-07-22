76f44081f7407edbfd7b18cbb4d2f55fe44980fe
saniasiddiqui <31842853+saniasiddiqui@users.noreply.github.com>
2020-07-22 12:17:16 +0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ if (config.env === 'production' || config.env === 'staging') {
 cron.schedule('*/5 * * * * *', SequenceScript.runSequenceMessageQueueScript) // after every five seconds
 cron.schedule('0 0 * * * *', TweetsQueueScript.deleteFromQueue) // daily at midnight
 cron.schedule('* * * * *', abandonedCartScript.runScript)
-cron.schedule('0 8 * * *', rssFeedsScript.runRSSScript) //  everyday at 8 AM
+//cron.schedule('0 8 * * *', rssFeedsScript.runRSSScript) //  everyday at 8 AM
 cron.schedule('0 13 * * *', rssFeedsScript.runRSSScript) //  daily 6 pm pakistan time
 cron.schedule('0 */2 * * *', manualFeedsScript.runScript)
 cron.schedule('* * * * *', sponsoredScheduled.runScheduleSponsored)
@@ -39,4 +39,4 @@ require('./config/express')(appObj)
 require('./config/setup')(app, httpApp, config)
 require('./routes')(appObj)
 require('./api/global/messageStatistics').connectRedis()
-require('./api/scripts/cpuProfiler')()
+//require('./api/scripts/cpuProfiler')()
