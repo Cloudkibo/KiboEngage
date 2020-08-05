1fbb7c1a34979e0adb1d7190e39a1aea9f828882
anishachhatwani <i130315@nu.edu.pk>
2020-08-05 12:56:41 +0500

    whatsapp abstraction

---
+++ server/whatsAppMapper/WhatsAppMapper.js
@@ -0,0 +1,24 @@
+const flockSend = require('../whatsAppMapper/flockSend/flockSend')
+const twilio = require('../whatsAppMapper/twilio/twilio')
+const {ActionTypes} = require('./constants')
+const providers = [
+  {key: 'flockSend', value: flockSend},
+  {key: 'twilio', value: twilio}
+]
+
+exports.whatsAppMapper = (provider, action, data, callback) => {
+  provider = providers.find(a => a.key === provider).value
+  return callAction(action, data, provider, callback)
+}
+
+function callAction (action, data, provider) {
+  switch (action) {
+    case ActionTypes.SEND_BROADCAST_MESSAGES:
+      return provider.sendBroadcastMessages(data)
+    case ActionTypes.GET_TEMPLATES:
+      return provider.getTemplates(data)
+    case ActionTypes.SEND_INVITATION_TEMPLATE:
+      return provider.sendInvitationTemplate(data)
+    default: break
+  }
+}
