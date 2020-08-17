2476d238a7223ef22b3cc092e99969947921db8e
anishachhatwani <i130315@nu.edu.pk>
2020-08-17 09:38:10 +0500

    graph in email

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -52,7 +52,7 @@ const all = {
     username: process.env.SENDGRID_USERNAME,
     password: process.env.SENDGRID_PASSWORD
   },
-  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || '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',
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   nodemailer: {
     service: 'gmail',
     email: 'muzamil@cloudkibo.com',
