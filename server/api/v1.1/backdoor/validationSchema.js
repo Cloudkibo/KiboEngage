b508ad47d433908c59b0cfa4ace8b78fe4131db0
anishachhatwani <i130315@nu.edu.pk>
2020-09-09 14:42:27 +0500

    acting user

--- server/api/v1.1/backdoor/validationSchema.js
+++ server/api/v1.1/backdoor/validationSchema.js
@@ -172,3 +172,19 @@ exports.commentCapturePayload = {
     }
   }
 }
+exports.actingAsUserPayload = {
+  properties: {
+    type: {
+      type: 'string',
+      required: true
+    },
+    domain_email: {
+      type: 'string',
+      required: false
+    },
+    name: {
+      type: 'string',
+      required: false
+    }
+  }
+}
