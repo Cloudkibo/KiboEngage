83777d7d7cc49373b758f1c1d8eaa5e138c5a326
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-10-19 15:35:38 +0500

    papertrail setup

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -36,5 +36,6 @@ module.exports = {
     kibodash: `${process.env.KIBODASH}/api/v1`
   },
   webhook_ip: process.env.WEBHOOK_IP_ADDRESS || 'localhost',
-  marketingApiAccessToken: 'EAAUTvApDOEYBAIDZA8niavVy4wTbNdFyJzkTaYFZCvGYi6lolruCFS8aBDZCkegQM1zoNAXUKAYZCDMbgANA9y0tFQLv4jiOKmDBsXeL8FMZAxkaXcwRJ3rrFHW5OkMAIXcdqUT9hb7IisM5J1xZB1dvh7a9yK7GbITD8M7ZBDsuAZDZD'
+  marketingApiAccessToken: 'EAAUTvApDOEYBAIDZA8niavVy4wTbNdFyJzkTaYFZCvGYi6lolruCFS8aBDZCkegQM1zoNAXUKAYZCDMbgANA9y0tFQLv4jiOKmDBsXeL8FMZAxkaXcwRJ3rrFHW5OkMAIXcdqUT9hb7IisM5J1xZB1dvh7a9yK7GbITD8M7ZBDsuAZDZD',
+  papertrail_log_levels: process.env.PAPERTRAIL_LOG_LEVELS
 }
