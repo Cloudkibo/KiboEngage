f0af8dc22d7d82271a9b0c12f41dddb7385dcfb0
arveen kumar <arveenkumar55@gmail.com>
2020-11-04 13:54:10 +0500

    fix_bug

--- scripts/whatsAppMessageStatus.js
+++ scripts/whatsAppMessageStatus.js
@@ -1,109 +1,86 @@
 const logger = require('../server/components/logger')
 const utility = require('../server/api/v1.1/utility')
 const TAG = 'scripts/whatsAppMessageStatus.js'
-const {updateCount} = require('../server/api/v1.1/flockSendEvents/controller')
 const async = require('async')
 
 exports.runScript = function () {
-  let query = {
-    purpose: 'findAll',
-    match: {platform: 'whatsApp'}
-  }
-  console.log('running script')
-  utility.callApi('queue/query', 'post', query, 'kiboengage')
-    .then(queues => {
-      console.log('queues got', queues[0])
-      queues.forEach(queue => {
-        utility.callApi('whatsAppBroadcastMessages/query',
-          'post', {purpose: 'findOne', match: {messageId: queue.payload.id}},
-          'kiboengage')
-          .then(message => {
-            if (message) {
-              updateCount(message, queue.payload)
-              deleteFromQueue(queue)
-            }
-          })
-          .catch(err => {
-            logger.serverLog(TAG, `Failed to fetch whatsAppBroadcastMessage ${err}`, 'error')
-          })
-      })
-    })
-    .catch(err => {
-      logger.serverLog(TAG, `Failed to fetch queues ${err}`, 'error')
-    })
+
+  console.log('runScript called')
+  updateAndDeleteMessages(0, 25, 'seen')
+  updateAndDeleteMessages(0, 25, 'delivered')
 }
 
-function uniqueValuesCount(arr) {
-  return arr.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
-}
-
-const updateAndDeleteMessages = function (skipRecords, LimitRecords) {
+const updateAndDeleteMessages = function (skipRecords, LimitRecords, status) {
   let query = {
     purpose: 'aggregate',
-    match: {},
+    match: {platform: 'whatsApp', 'payload.status': status},
     skip: skipRecords,
     limit: LimitRecords
   }
-  utility.callApi('queue/aggregate', 'post', query, 'kiboengage')
+  utility.callApi('queue/query', 'post', query, 'kiboengage')
     .then(queues => {
       if (queues.length > 0) {
         let queuesIds = queues.map(queue => queue.payload.id)
-        let uniqueQueuesIds = uniqueValuesCount(queuesIds)
+        console.log('queuesIds', queuesIds)
         utility.callApi('whatsAppBroadcastMessages/query',
-          'post', {purpose: 'findAll', match: {messageId: {$in: Object.keys(uniqueQueuesIds)}}},
+          'post', {purpose: 'findAll', match: {messageId: {$in: queuesIds}}},
           'kiboengage')
           .then(messages => {
-           _updateCount(messages, uniqueQueuesIds).then(data => {
-            })
-        })
-        // _updateCount(queues).then(data => {
-        //   let queueIds = queues.map(queue => queue.payload.id)
-        //   deleteFromQueue(queueIds).then(data => {
-        //     updateAndDeleteMessages(skipRecords + LimitRecords, LimitRecords)
-        //   }).catch(err => {
-        //     logger.serverLog(TAG, `Failed to delete broadcast messages from Queue ${err}`, 'error')
-        //   })
-
-        // }).catch(err => {
-        //   logger.serverLog(TAG, `Failed to updtae broadcast messages ${err}`, 'error')
-        // })
+            if (messages.length > 0) {
+              console.log('messages', messages)
+              _updateCount(messages, status).then(data => {
+                updateAndDeleteMessages(skipRecords + LimitRecords, LimitRecords, status)
+              })
+            }
+          })
       } // else we don't need to do anything
     })
     .catch(err => {
-      logger.serverLog(TAG, `Failed to fetch queues ${err}`, 'error')
+      logger.serverLog(TAG, `Failed to fetch queues ${JSON.stringify(err)}`, 'error')
     })
 }
 
-const _updateCount = (queues) => {
+const _updateCount = (messages, status) => {
   return new Promise(function (resolve, reject) {
     let requests = []
-    queues.forEach(queue => {
-      requests.push(
-        utility.callApi('whatsAppBroadcastMessages/query',
-          'post', {purpose: 'findOne', match: {messageId: queue.payload.id}},
-          'kiboengage')
-          .then(message => {
-            if (message) {
-              return new Promise((resolve, reject) => {
-                async.parallelLimit([
-                  _UpdatewhatsAppBroadcastMessages.bind(null, message, queue.payload),
-                  _UpdatewhatsAppBroadcasts.bind(null, message, queue.payload)
-                ], 10, function (err, results) {
-                  if (err) {
-                    logger.serverLog(TAG, `Failed to fetch broadcasts ${JSON.stringify(err)}`, 'error')
-                  } else {
-                  }
-                  resolve('success')
-                })
-              })
-              // updateCount(message, queue.payload)
-              // deleteFromQueue(queue)
-            }
+    let broadcastIds = []
+    messages.forEach(message => {
+      let broadcastId = message.broadcastId
+      let broadcastCount = messages.filter(messageTemp => messageTemp.broadcastId === broadcastId).length
+      console.log('broadcastCount', broadcastCount)
+      if (broadcastIds.includes(broadcastId)) {
+        requests.push(
+          new Promise((resolve, reject) => {
+            async.parallelLimit([
+              _UpdatewhatsAppBroadcastMessages.bind(null, message, status),
+              deleteFromQueue.bind(null, message.messageId, status)
+            ], 10, function (err, results) {
+              if (err) {
+                logger.serverLog(TAG, `Failed to update broadcasts in whatsapp Message status file ${JSON.stringify(err)}`, 'error')
+              } else {
+              }
+              resolve('success')
+            })
           })
-          .catch(err => {
-            logger.serverLog(TAG, `Failed to fetch whatsAppBroadcastMessage ${err}`, 'error')
+        )
+      } else {
+        broadcastIds.push(broadcastId)
+        requests.push(
+          new Promise((resolve, reject) => {
+            async.parallelLimit([
+              _UpdatewhatsAppBroadcastMessages.bind(null, message, status),
+              _UpdatewhatsAppBroadcasts.bind(null, message, status, broadcastCount),
+              deleteFromQueue.bind(null, message.messageId, status)
+            ], 10, function (err, results) {
+              if (err) {
+                logger.serverLog(TAG, `Failed to fetch broadcasts ${JSON.stringify(err)}`, 'error')
+              } else {
+              }
+              resolve('success')
+            })
           })
-      )
+        )
+      }
     })
     Promise.all(requests)
       .then(results => {
@@ -111,48 +88,50 @@ const _updateCount = (queues) => {
       })
   }) 
 }
-function deleteFromQueue (queueIds) {
-  return new Promise(function (resolve, reject) {
-    utility.callApi(
-      'queue',
-      'delete',
-      {purpose: 'deleteMany', match: {_id: {$in: queueIds}}},
-      'kiboengage')
-      .then(deleted => {
-        resolve('success')
-      })
-      .catch(err => {
-        resolve('success')
-        logger.serverLog(TAG, `Failed to delete tweet from tweets queue ${err}`, 'error')
-      })
-  })
+function deleteFromQueue (messageId, status, next) {
+  utility.callApi(
+    'queue',
+    'delete',
+    {purpose: 'deleteOne', match: {'payload.id': messageId, 'payload.status': status}},
+    'kiboengage')
+    .then(deleted => {
+      next(null, deleted)
+    })
+    .catch(err => {
+      next(err)
+      logger.serverLog(TAG, `Failed to delete whatsapp message from tweets queue ${err}`, 'error')
+    })
 }
 
-const _UpdatewhatsAppBroadcastMessages = (message, body) => {
+const _UpdatewhatsAppBroadcastMessages = (message, status, next) => {
   let updateData = {
     purpose: 'updateOne',
     match: {_id: message._id},
-    updated: body.status === 'delivered' ? {delivered: true} : {seen: true}
+    updated: status === 'delivered' ? {delivered: true} : {seen: true}
   }
   utility.callApi(`whatsAppBroadcastMessages`, 'put', updateData, 'kiboengage')
     .then(message => {
+      next(null, message)
     })
     .catch((err) => {
+      next(err)
       logger.serverLog(`Failed to update message ${err}`, 'error')
     })
 }
 
-const _UpdatewhatsAppBroadcasts = (message, body) => {
+const _UpdatewhatsAppBroadcasts = (message, status, incValue, next) => {
   let broadcastCountUpdate = {
     purpose: 'updateOne',
     match: {_id: message.broadcastId},
-    updated: body.status === 'delivered'
-      ? { $inc: { delivered: 1 } } : { $inc: { seen: 1 } }
+    updated: status === 'delivered'
+      ? { $inc: { delivered: incValue } } : { $inc: { seen: incValue } }
   }
   utility.callApi(`whatsAppBroadcasts`, 'put', broadcastCountUpdate, 'kiboengage')
     .then(broadcast => {
+      next(null, broadcast)
     })
     .catch((err) => {
+      next(err)
       logger.serverLog(`Failed to update broadcast ${err}`, 'error')
     })
 }
